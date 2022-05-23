b438a028f2e5f333db83bdd9960bb023a8b6eb5c
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-05-23 21:03:58 +0300

    .

---
+++ C# Fundamentals/01. Basic Syntax, Conditional Statements and Loops/Lab/08. Divisible by 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08._Divisible_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //08. Divisible by 3
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/01. Basic Syntax, Conditional Statements and Loops/Lab/09. Sum Of Odd Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09._Sum_Of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //09. Sum Of Odd Numbers
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n * 2; i += 2)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
