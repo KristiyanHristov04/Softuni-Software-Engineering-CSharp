7615f1de2643f0ecb6775a04ed3231bb24038714
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-05-23 21:05:35 +0300

    .

---
+++ C# Fundamentals/01. Basic Syntax, Conditional Statements and Loops/Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //10. Multiplication Table
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}
