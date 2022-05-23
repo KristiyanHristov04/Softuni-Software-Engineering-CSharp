58f77fca00155e4088de5261e8937e04cc7ce178
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-05-23 21:09:33 +0300

    .

---
+++ C# Fundamentals/01. Basic Syntax/Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //11. Multiplication Table 2.0
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fromWhere = Convert.ToInt32(Console.ReadLine());
+            bool isValid = false;
+            for (int i = fromWhere; fromWhere <= 10; fromWhere++)
+            {
+                Console.WriteLine($"{n} X {fromWhere} = {n * fromWhere}");
+                isValid = true;
+            }
+
+            if (fromWhere > 10 && isValid == false)
+            {
+                Console.WriteLine($"{n} X {fromWhere} = {n * fromWhere}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/01. Basic Syntax/Lab/12. Even Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12. Even Number
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            number = Math.Abs(number);
+            Console.WriteLine($"The number is: {number}");
+        }
+    }
+}
