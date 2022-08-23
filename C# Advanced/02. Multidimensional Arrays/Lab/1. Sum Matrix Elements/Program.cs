ef743a567508ad15f7a6aed7f7f16631cda4a296
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-08-23 17:19:27 +0300

    .

---
+++ C# Advanced/02. Multidimensional Arrays/Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            int[,] matrix = new int[rows, cols];
+            int result = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] currColumn = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = currColumn[j];    
+                    result += currColumn[j];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(result);
+        }
+    }
+}
