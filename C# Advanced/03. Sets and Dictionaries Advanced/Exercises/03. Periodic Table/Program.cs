6fafaa9b07983a38f2107e41cb3d83f876c01c1e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:11:42 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Exercises/03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            SortedSet<string> set = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (!set.Contains(input[j]))
+                    {
+                        set.Add(input[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", set));
+        }
+    }
+}
