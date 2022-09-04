94f5e70fadc8503e5f61ccc696ddb9ca0a6a2204
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:11:30 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Exercises/01. Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
