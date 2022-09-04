edfacd11bb8373da91ffbb17887933551af3c9d1
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:09:46 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            numbers = numbers.OrderByDescending(x => x).ToList();
+            if (numbers.Count < 3)
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            
+        }
+    }
+}
