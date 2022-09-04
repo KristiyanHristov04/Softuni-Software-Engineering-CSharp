a731c5f47920b4c2076a1a0f1d926535cec019d1
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:10:09 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
