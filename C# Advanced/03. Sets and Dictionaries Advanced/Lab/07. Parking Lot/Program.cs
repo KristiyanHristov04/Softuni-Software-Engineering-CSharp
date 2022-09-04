443d058bfa860e84d037f2ad2363bab2507a2bed
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:10:16 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                string[] commands = input.Split(", ");
+                string typeOfAction = commands[0];
+                string plateNumber = commands[1];
+                switch (typeOfAction)
+                {
+                    case "IN":
+                        parkingLot.Add(plateNumber);
+                        break;
+                    case "OUT":
+                        parkingLot.Remove(plateNumber);
+                        break;
+                }
+            }
+
+            if (parkingLot.Count > 0)
+            {
+                foreach (var plateNumber in parkingLot)
+                {
+                    Console.WriteLine(plateNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+        }
+    }
+}
