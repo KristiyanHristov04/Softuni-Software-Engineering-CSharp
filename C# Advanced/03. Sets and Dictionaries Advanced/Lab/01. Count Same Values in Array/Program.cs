e838edbcea831b594debb4631d590d79f9a8be91
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:09:31 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> numbersCount = new Dictionary<double, int>();
+            double[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (numbersCount.ContainsKey(input[i]))
+                {
+                    numbersCount[input[i]]++;
+                }
+                else
+                {
+                    numbersCount.Add(input[i], 1);
+                }
+            }
+
+            foreach (var number in numbersCount)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
