5b4c6dfdbf140804c03d91c9e063aa0a7da23c22
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-04 12:11:53 +0300

    .

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/Exercises/05. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            SortedDictionary<char, int> myDictionary = new SortedDictionary<char, int>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (myDictionary.ContainsKey(text[i]))
+                {
+                    myDictionary[text[i]]++;
+                }
+                else
+                {
+                    myDictionary.Add(text[i], 1);
+                }
+            }
+
+            foreach (var ch in myDictionary)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
