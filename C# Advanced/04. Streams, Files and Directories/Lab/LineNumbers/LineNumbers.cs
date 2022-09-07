58fa0e53740a5ca4c2e0adaed02b02b608d7ebc0
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-07 12:40:38 +0300

    .

---
+++ C# Advanced/04. Streams, Files and Directories/Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,39 @@
+﻿namespace LineNumbers
+{
+    using System.Collections.Generic;
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            Queue<string> text = new Queue<string>();
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    string line = reader.ReadLine();
+                    text.Enqueue(line);
+                }
+            }
+
+            int counter = 1;
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                while (text.Count > 0)
+                {
+                    string textLine = text.Dequeue();
+                    writer.WriteLine($"{counter}. {textLine}");
+                    counter++;
+                }
+            }
+        }
+    }
+}
