d1a1cd1880ce752dd6b6b738db180cce72c8b37e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-07 12:41:15 +0300

    .

---
+++ C# Advanced/04. Streams, Files and Directories/Lab/ExtractSpecialBytes/ExtractBytes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ExtractBytes
+{
+    using System;
+    using System.IO;
+    public class ExtractBytes
+    {
+        static void Main(string[] args)
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using StreamReader reader = new StreamReader(bytesFilePath);
+            byte[] fileBytes = File.ReadAllBytes(binaryFilePath);
+            List<string> bytesList = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            while (!reader.EndOfStream)
+            {
+                bytesList.Add(reader.ReadLine());
+            }
+
+            foreach (var item in fileBytes)
+            {
+                if (bytesList.Contains(item.ToString()))
+                {
+                    sb.AppendLine(item.ToString());
+                }
+            }
+
+            using StreamWriter writer = new StreamWriter(outputPath);
+            writer.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
