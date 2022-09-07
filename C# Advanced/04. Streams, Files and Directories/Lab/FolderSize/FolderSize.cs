aca65770ee836a175b98dc5f0e7e1877c2c20276
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-07 12:41:24 +0300

    .

---
+++ C# Advanced/04. Streams, Files and Directories/Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,30 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+            FileInfo[] infos = dirInfo.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach (var fileInfo in infos)
+            {
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+            File.WriteAllText(outputFilePath, sum.ToString());
+        }
+    }
+}
