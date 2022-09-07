df4d9e6108128a84a8c6e3333e22b0d4689d4d39
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-07 12:43:01 +0300

    .

---
+++ C# Advanced/04. Streams, Files and Directories/Exercises/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,37 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class CopyDirectory
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = Console.ReadLine();
+            string outputPath = Console.ReadLine();
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath);
+            }
+            Directory.CreateDirectory(outputPath);
+
+            var files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(fileName, copyDestination);
+            }
+        }
+    }
+}
