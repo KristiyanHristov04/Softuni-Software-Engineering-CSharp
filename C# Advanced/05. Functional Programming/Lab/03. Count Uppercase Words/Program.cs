04bc6e59ccb9d47f841c706606a8b63e0dc7c930
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-09 20:29:20 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> result = new Queue<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string curretWord = text[i];
+                char currentWordFirstChar = curretWord[0];
+                if (char.IsUpper(currentWordFirstChar))
+                {
+                    result.Enqueue(curretWord);
+                }
+            }
+
+            while (result.Count > 0)
+            {
+                Console.WriteLine(result.Dequeue());
+            }
+        }
+    }
+}
