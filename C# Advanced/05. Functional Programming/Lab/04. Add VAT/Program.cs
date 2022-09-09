18adb213857e6f2267609be87e848d2179ea52ed
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-09 20:29:27 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Lab/04. Add VAT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join("", Console.ReadLine().Split(", ").Select(double.Parse).Select(x => $"{ x + x * 0.20:f2}\n").ToArray()).TrimEnd());
+        }
+    }
+}
