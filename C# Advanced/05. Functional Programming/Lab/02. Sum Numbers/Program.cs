e51df0b2464979fd8d3d5172c58382aba1b86aad
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-09 20:29:13 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
