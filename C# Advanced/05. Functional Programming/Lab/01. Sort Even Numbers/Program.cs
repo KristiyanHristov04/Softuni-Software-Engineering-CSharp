97c13af6457558ec79ec8980ecb1419570b15b93
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-09 20:29:06 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(", ",Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray()));
+        }
+    }
+}
