2f83313fc5fc76dc6cc4f09f7e1faebcd835bd44
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 21:58:45 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/01. Action Print/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printNames = name => Console.WriteLine(name);
+            string[] input = Console.ReadLine().Split(' ');
+            for (int i = 0; i < input.Length; i++)
+            {
+                printNames(input[i]);
+            }
+        }
+    }
+}
