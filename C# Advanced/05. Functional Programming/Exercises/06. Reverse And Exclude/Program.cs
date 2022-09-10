8ca77988fc1fc00f04084553d2c2893e40cda4ac
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 21:59:12 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = Convert.ToInt32(Console.ReadLine());
+            Func<int[], int[]> reverseCollection = numbers => numbers.Reverse().ToArray();
+            Predicate<int> isNotDivisible = number => number % n != 0;
+
+            numbers = reverseCollection(numbers);
+            Console.WriteLine(String.Join(" ", numbers.Where(x => isNotDivisible(x))));
+        }
+    }
+}
