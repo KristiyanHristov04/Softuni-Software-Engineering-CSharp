ee6679a863402399a6a4285f1ccc85cddb031be5
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 21:59:01 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int minRange = input[0];
+            int maxRange = input[1];
+            string condition = Console.ReadLine();
+            Predicate<int> isEven = x => x % 2 == 0;
+            Predicate<int> isOdd = x => x % 2 != 0;
+            List<int> result = new List<int>();
+            if (condition == "even")
+            {
+                for (int i = minRange; i <= maxRange; i++)
+                {
+                    result.Add(i);
+                }
+                result = result.FindAll(isEven);
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else if (condition == "odd")
+            {
+                for (int i = minRange; i <= maxRange; i++)
+                {
+                    result.Add(i);
+                }
+                result = result.FindAll(isOdd);
+                Console.WriteLine(string.Join(" ", result));
+            }
+        }
+    }
+}
