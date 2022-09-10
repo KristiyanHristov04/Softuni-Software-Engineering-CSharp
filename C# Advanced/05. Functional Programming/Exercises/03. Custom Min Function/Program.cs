4c1bff3c6f6f3d8690b4ceaebe8e3b6316b38dc4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 21:58:56 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/03. Custom Min Function/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Func<int[], int> smallestNumber = numbers => numbers.Min();
+            Console.WriteLine(smallestNumber(numbers));
+        }
+    }
+}
