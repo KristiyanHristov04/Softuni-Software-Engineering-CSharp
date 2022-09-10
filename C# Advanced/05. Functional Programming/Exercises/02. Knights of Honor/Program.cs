31e53da0a9baef59e9a7074422cb7d6d30fd1c62
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 21:58:50 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/02. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            Func<string, string> printNames = name => "Sir " + name;
+            foreach (var name in input)
+            {
+                Console.WriteLine(printNames(name));
+            }
+        }
+    }
+}
