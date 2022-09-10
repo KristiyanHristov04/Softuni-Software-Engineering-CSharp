7307917c7178ef093053b6add72bd085b81923fa
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 22:01:21 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/11. TriFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int threshold = Convert.ToInt32(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            Func<int, bool> function = sum => sum >= threshold;
+            Console.WriteLine(FindName(names, function));
+        }
+        public static string FindName(string[] names, Func<int, bool> functionParameter)
+        {
+            string returningValue = string.Empty;
+            for (int i = 0; i < names.Length; i++)
+            {
+                int sum = 0;
+                string currentName = names[i];
+                char[] charredName = currentName.ToCharArray();
+                for (int j = 0; j < charredName.Length; j++)
+                {
+                    sum += charredName[j];
+                }
+                if (functionParameter(sum))
+                {
+                    returningValue = currentName;
+                    break;
+                }
+            }
+            return returningValue;
+        }
+    }
+}
