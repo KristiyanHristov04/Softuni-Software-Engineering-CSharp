bd9d33baef0ac215ba8d3d88b1c06e81ea13e472
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-10 21:59:18 +0300

    .

---
+++ C# Advanced/05. Functional Programming/Exercises/07. Predicate For Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLength = Convert.ToInt32(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            Predicate<string> isInBounds = name => name.Length <= maxLength;
+            foreach (var name in names.Where(name => isInBounds(name)))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
