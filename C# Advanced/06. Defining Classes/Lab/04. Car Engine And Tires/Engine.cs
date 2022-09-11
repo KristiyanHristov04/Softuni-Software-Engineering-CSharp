ccf42c65a9439d908b4965ce6fba75bd3ee4d467
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-11 16:33:08 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Lab/04. Car Engine And Tires/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsePower;
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        private double cubicCapacity;
+
+        public double CubicCapacity
+        {
+            get { return cubicCapacity; }
+            set { cubicCapacity = value; }
+        }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+    }
+}
---
+++ C# Advanced/06. Defining Classes/Lab/04. Car Engine And Tires/Tire.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        int year;
+        double pressure;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+    }
+}
