b75c6eb00bdda49ce8c9f009126211c3d26b51f9
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-11 16:33:02 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Lab/03. Car Constructors/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _03._Car_Constructors;
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = Convert.ToInt32(Console.ReadLine());
+            double fuelQuantity = Convert.ToDouble(Console.ReadLine());
+            double fuelConsumption = Convert.ToDouble(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);  
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+        }
+    }
+}
