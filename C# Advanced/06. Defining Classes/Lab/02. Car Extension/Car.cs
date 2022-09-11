3937676a518aad6dd893cdfbd5c76e914070216f
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-11 16:32:53 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Lab/02. Car Extension/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Car_Extension
+{
+    internal class Car
+    {
+        string make;
+        string model;
+        int year;
+        double fuelQuantity;
+        double fuelConsumption;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - distance * this.FuelConsumption > 0)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}";
+        }
+    }
+}
