0d80bda97f39f0fafd73db36734ee0dc6c37309e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-11 16:32:46 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Lab/01. Car/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Car
+{
+    internal class Car
+    {
+        string make;
+        string model;
+        int year;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+    }
+}
