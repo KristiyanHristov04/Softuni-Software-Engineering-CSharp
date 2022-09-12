08a5a65be4a0d153d108af79206be4dd03432509
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-12 23:45:59 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Exercises/02. Creating Constructors/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person()
+        {
+            name = "No name";
+            age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ C# Advanced/06. Defining Classes/Exercises/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", 12);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
