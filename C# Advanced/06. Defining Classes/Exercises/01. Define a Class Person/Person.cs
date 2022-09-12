29052fc2585b1f01f36b4716ed125ed0bf8cd28d
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-12 23:45:53 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Exercises/01. Define a Class Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Person
+    {
+        string name;
+        int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
