6d6024713eab2c7b943bdd4ff1c8d3cdd1fea8da
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-12 23:46:06 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Exercises/03. Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        List<Person> people = new List<Person>();
+
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+        
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        public Person GetOldestMember(List<Person> people)
+        {
+            int oldestAge = people.Max(p => p.Age);
+            return people.First(p => p.Age == oldestAge);
+        }
+    }
+}
---
+++ C# Advanced/06. Defining Classes/Exercises/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DefiningClasses;
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = Convert.ToInt32(input[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember(family.People);
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
