f59fac31a4a650634e6032e2422e2b962c8137d0
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-13 12:19:08 +0300

    .

---
+++ C# Advanced/06. Defining Classes/Exercises/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public static int CalculateDifference(string firstDate, string secondDate)
+        {
+            DateTime first = DateTime.Parse(firstDate);
+            DateTime second = DateTime.Parse(secondDate);
+
+            int result = Convert.ToInt32((first - second).TotalDays);
+
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/06. Defining Classes/Exercises/05. Date Modifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            int result = DateModifier.CalculateDifference(firstDate, secondDate);
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
