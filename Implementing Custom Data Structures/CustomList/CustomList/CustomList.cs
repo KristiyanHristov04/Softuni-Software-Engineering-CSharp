bb05d5208f95c23b43379cfa2e9c92b44e4c5af2
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-16 19:05:59 +0300

    .

---
+++ Implementing Custom Data Structures/CustomList/CustomList/CustomList.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+        public int Count { get; private set; }
+        public CustomList()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int this[int index]
+        {
+            get 
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentException();
+                }
+                return items[index];
+            }
+            set
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentException();
+                }
+                items[index] = value;
+            }
+        }
+
+        public void Add(int item)
+        {
+            if (this.Count == this.items.Length)
+            {
+                Resize();
+            }
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+        public int RemoveAt(int index)
+        {
+            if (index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var item = this.items[index];
+            this.items[index] = default(int);
+            this.Shift(index);
+            this.Count--;
+
+            if (this.Count <= this.items.Length / 4)
+            {
+                Shrink();
+            }
+
+            return item;
+        }
+        public void Insert(int index, int element)
+        {
+            if (index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+            ShiftToRight(index);
+            items[index] = element;
+            Count++;
+        }
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if ((firstIndex < this.Count || secondIndex < this.Count) && firstIndex >= 0 && secondIndex >= 0)
+            {
+                int firstNumber = this.items[firstIndex];
+                int secondNumber = this.items[secondIndex];
+
+                this.items[firstIndex] = secondNumber;
+                this.items[secondIndex] = firstNumber;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < Count - 1; i++)
+            {
+                sb.Append(items[i] + ", ");
+            }
+
+            if (Count != 0)
+            {
+                sb.Append(items[Count - 1]);
+            }
+
+            return sb.ToString();
+        }
+        private void Resize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+            this.items = copy;
+        }
+        private void Shift(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+        }
+        private void Shrink()
+        {
+            int[] copy = new int[this.items.Length / 2];
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+            this.items = copy;
+        }
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+        }
+    }
+}
---
+++ Implementing Custom Data Structures/CustomList/CustomList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+            list.Add(5);
+            list.Add(50);
+            list.Add(500);
+            list.Add(5000);
+            Console.WriteLine(String.Join(", ", list));
+            list.Swap(1, 2);
+            Console.WriteLine(String.Join(", ", list));
+            Console.WriteLine(list.Contains(50));
+            list.RemoveAt(2);
+            Console.WriteLine(list.Contains(50));
+            Console.WriteLine(String.Join(", ", list));
+            list.Insert(3, 50000);
+            Console.WriteLine(String.Join(", ", list));
+        }
+    }
+}
