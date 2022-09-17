2760f76cfbc5a007f4fdffb9aaa09afd9fe060f7
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-17 14:30:32 +0300

    .

---
+++ Implementing Custom Data Structures/CustomQueue/CustomQueue/CustomQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomQueue
+{
+    internal class CustomQueue
+    {
+        private int[] items;
+        private const int InitialCapacity = 4;
+        public int Count { get; private set; }
+        public CustomQueue()
+        {
+            this.Count = 0;
+            this.items = new int[InitialCapacity];
+        }
+
+        public void Enqueue(int element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                IncreaseSize();
+            }
+            this.items[Count] = element;
+            this.Count++;
+        }
+        public void Dequeue()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentException("CustomQueue is Empty.");
+            }
+            int[] copy = new int[this.Count - 1];
+            for (int i = 0; i < copy.Length; i++)
+            {
+                copy[i] = this.items[i + 1];
+            }
+            this.items = copy;
+            this.Count--;
+        }
+        public void Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentException("CustomQueue is Empty. This operation cannot be executed because the CustomQueue is empty.");
+            }
+            Console.WriteLine(this.items[0]);
+        }
+        public void Clear()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentException("This command cannot be executed because the CustomQueue is empty already.");
+            }
+            this.Count = 0;
+            this.items = new int[InitialCapacity];
+        }
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+        private void IncreaseSize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.items[i];
+            }
+            this.items = copy;
+        }
+    }
+}
---
+++ Implementing Custom Data Structures/CustomQueue/CustomQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CustomQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue queue = new CustomQueue();
+            queue.Enqueue(1);
+            queue.Enqueue(10);
+            queue.Enqueue(100);
+            queue.Dequeue();
+            queue.Peek();
+            queue.ForEach(x => Console.WriteLine(x));
+            queue.Clear();
+        }
+    }
+}
