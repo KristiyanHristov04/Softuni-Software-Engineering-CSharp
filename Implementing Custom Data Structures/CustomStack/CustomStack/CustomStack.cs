a3fb3b8254c8a050a941621012313f5b1746e02f
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-17 14:30:38 +0300

    .

---
+++ Implementing Custom Data Structures/CustomStack/CustomStack/CustomStack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class CustomStack
+    {
+        private int[] items;
+        private const int InitialCapacity = 4;
+        public int Count { get; private set; }
+        public CustomStack()
+        {
+            this.Count = 0;
+            this.items = new int[InitialCapacity];
+        }
+        public void Push(int element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                var nextItems = new int[this.items.Length * 2];
+                for (int i = 0; i < this.items.Length; i++)
+                {
+                    nextItems[i] = this.items[i];
+                }
+                this.items = nextItems;
+            }
+            this.items[this.Count] = element;
+            Count++;
+        }
+        public int Pop()
+        {
+            if (this.Count == 0)
+            {
+                //Throw exception
+                throw new ArgumentException("CustomStack is empty");
+            }
+            var lastIndex = this.Count - 1;
+            int last = this.items[lastIndex];
+            this.Count--;
+            return last;
+        }
+        public void Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentException("Stack is empty.");
+            }
+            int firstElement = this.items[0];
+            Console.WriteLine(firstElement);
+        }
+        public void Contains(int element)
+        {
+            bool doesExist = false;
+            foreach (var number in this.items)
+            {
+                if (number == element)
+                {
+                    doesExist = true;
+                }
+            }
+            Console.WriteLine(doesExist);
+        }
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var number in this.items)
+            {
+                sb.Append(number + " ");
+            }
+            return sb.ToString();
+        }
+        private void Resize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.items[i];
+            }
+        }
+    }
+}
---
+++ Implementing Custom Data Structures/CustomStack/CustomStack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CustomStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+            stack.Push(5);
+            stack.Push(29);
+            stack.Push(15);
+            Console.WriteLine(stack);
+            stack.Pop();
+            Console.WriteLine(stack);
+            stack.Peek();
+            stack.Contains(51);
+        }
+    }
+}
