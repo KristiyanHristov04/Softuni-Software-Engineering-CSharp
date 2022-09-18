2d08bdefe8c0430a2abdc38d3d7ce71ba6acaca4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-18 19:39:19 +0300

    .

---
+++ C# Advanced/07. Generics/Lab/03. Generic Scale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //var areEqual = EqualityScale<int>.Equals(15, 15);
+            //Console.WriteLine(areEqual);
+        }
+    }
+}
