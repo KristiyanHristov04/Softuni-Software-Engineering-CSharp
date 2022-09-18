aff49b46d689a9671ad6b6007d01b0e7feff77e0
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-18 19:39:08 +0300

    .

---
+++ C# Advanced/07. Generics/Lab/01. Box/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> List { get; private set; }
+        public Box()
+        {
+            this.List = new List<T>();
+        }
+        public int Count { get { return List.Count; } }
+        public void Add(T element)
+        {
+            this.List.Insert(0, element);
+        }
+        public T Remove()
+        {
+            if (this.List.Count == 0)
+            {
+                throw new ArgumentException("Box is empty. Invalid Operation.");
+            }
+            T elementToReturn = this.List[0];
+            this.List.RemoveAt(0);
+            return elementToReturn;
+        }
+    }
+}
---
+++ C# Advanced/07. Generics/Lab/01. Box/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            box.Add("Pesho");
+            box.Add("Toni");
+            box.Add("Blagoi");
+            Console.WriteLine(box.Remove());
+            box.Add("Ivan");
+            box.Add("Angel");
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
