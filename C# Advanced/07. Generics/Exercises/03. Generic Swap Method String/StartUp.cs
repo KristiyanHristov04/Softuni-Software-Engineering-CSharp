2f206ed8fa9de5a10795a9c9d984238bf931b651
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-19 18:37:43 +0300

    .

---
+++ C# Advanced/07. Generics/Exercises/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Generic_Swap_Method_String
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+            int[] indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+
+        }
+    }
+}
