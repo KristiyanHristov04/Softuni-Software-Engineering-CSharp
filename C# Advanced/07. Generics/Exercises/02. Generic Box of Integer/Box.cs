b26c759a1672b7d4b75b5cbbac5d3d985e46c6fc
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-19 18:37:38 +0300

    .

---
+++ C# Advanced/07. Generics/Exercises/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+        public T Element { get; set; }
+        public Box(T value)
+        {
+            this.Element = value;
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.Element}";
+        }
+    }
+}
---
+++ C# Advanced/07. Generics/Exercises/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Generic_Box_of_Integer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                var box = new Box<int>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
