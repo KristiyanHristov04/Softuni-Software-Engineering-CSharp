c64195e31bb4c4dd8cf4612e3ef4e91cf3d60edf
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-20 17:16:15 +0300

    .

---
+++ C# Advanced/07. Generics/Exercises/07. Tuple/MyTuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Tuple
+{
+    public class MyTuple<T1, T2>
+    {
+        public MyTuple(T1 t1, T2 t2)
+        {
+            this.Item1 = t1;
+            this.Item2 = t2;
+        }
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
---
+++ C# Advanced/07. Generics/Exercises/07. Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string firstName = input.Split(' ')[0];
+            string secondName = input.Split(' ')[1];
+            string adress = input.Split(' ')[2];
+            string fullName = firstName + " " + secondName;
+            MyTuple<string, string> tuple01 = new MyTuple<string, string>(fullName, adress);
+            Console.WriteLine(tuple01);
+
+            string secondInput = Console.ReadLine();
+            string name = secondInput.Split(' ')[0];
+            int litersOfBeer = Convert.ToInt32(secondInput.Split(' ')[1]);
+            MyTuple<string, int> tuple02 = new MyTuple<string, int>(name, litersOfBeer);
+            Console.WriteLine(tuple02);
+
+            string thirdInput = Console.ReadLine();
+            int integer = Convert.ToInt32(thirdInput.Split(' ')[0]);
+            double doubleNum = Convert.ToDouble(thirdInput.Split(' ')[1]);
+            MyTuple<int, double> tuple03 = new MyTuple<int, double>(integer, doubleNum);
+            Console.WriteLine(tuple03);
+        }
+    }
+}
