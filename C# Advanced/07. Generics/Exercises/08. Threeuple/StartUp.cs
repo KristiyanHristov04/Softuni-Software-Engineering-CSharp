66f9c9ef17234f50c9274a0372960cd3405d13d1
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-21 18:00:20 +0300

    .

---
+++ C# Advanced/07. Generics/Exercises/08. Threeuple/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08._Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            if (firstInput.Length == 4)
+            {
+                string firstName = firstInput[0];
+                string lastName = firstInput[1];
+                string adress = firstInput[2];
+                string town = firstInput[3];
+
+                string fullName = firstName + " " + lastName;
+                Threeuple<string, string, string> tuple01 = new Threeuple<string, string, string>(fullName, adress, town);
+                Console.WriteLine(tuple01);
+            }
+            else
+            {
+                string firstName = firstInput[0];
+                string lastName = firstInput[1];
+                string adress = firstInput[2];
+                string town01 = firstInput[3];
+                string town02 = firstInput[4];
+
+                string fullName = firstName + " " + lastName;
+                string fullTownName = town01 + " " + town02;
+                Threeuple<string, string, string> tuple01 = new Threeuple<string, string, string>(fullName, adress, fullTownName);
+                Console.WriteLine(tuple01);
+            }
+            
+
+            string[] secondInput = Console.ReadLine().Split();
+            string name = secondInput[0];
+            int litersOfBeer = Convert.ToInt32(secondInput[1]);
+            string drunkOrNot = secondInput[2] == "drunk" ? "True" : "False";
+            Threeuple<string, int, string> tuple02 = new Threeuple<string, int, string>(name, litersOfBeer, drunkOrNot);
+            Console.WriteLine(tuple02);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            string name02 = thirdInput[0];
+            double accountBalance = Convert.ToDouble(thirdInput[1]);
+            string bankName = thirdInput[2];
+            Threeuple<string, double, string> tuple03 = new Threeuple<string, double, string>(name02, accountBalance, bankName);
+            Console.WriteLine(tuple03);
+
+        }
+    }
+}
---
+++ C# Advanced/07. Generics/Exercises/08. Threeuple/Threeuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08._Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple(T1 t1, T2 t2, T3 t3)
+        {
+            this.Item1 = t1;
+            this.Item2 = t2;
+            this.Item3 = t3;
+        }
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+    }
+}
