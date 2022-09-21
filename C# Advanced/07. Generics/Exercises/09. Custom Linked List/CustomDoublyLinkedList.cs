cf06db13c46fe40db6f75bca0471f582c4ce98fa
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-21 18:00:27 +0300

    .

---
+++ C# Advanced/07. Generics/Exercises/09. Custom Linked List/CustomDoublyLinkedList.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class CustomDoublyLinkedList<T>
+    {
+        private class ListNode
+        {
+            public T Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+        }
+        private ListNode head;
+        private ListNode tail;
+        public int Count { get; private set; }
+        public void AddFirst(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+            Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                var newTail = new ListNode(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+            int counter = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+            return array;
+        }
+
+        public int Length()
+        {
+            return this.Count;
+        }
+    }
+}
---
+++ C# Advanced/07. Generics/Exercises/09. Custom Linked List/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomDoublyLinkedList<int> list = new CustomDoublyLinkedList<int>();
+            list.AddFirst(1);
+            list.AddFirst(2);
+            Console.WriteLine(list.Length());
+            Console.WriteLine(list.RemoveLast());
+            Console.WriteLine(list.RemoveFirst());
+            list.AddFirst(10);
+            list.AddFirst(5);
+            list.AddFirst(24);
+            list.ForEach(x => Console.WriteLine(x));
+            int[] array = list.ToArray();
+            Predicate<int> predicate = number => number % 2 == 0;
+            array = array.Where(number => predicate(number)).ToArray();
+            Console.WriteLine("All even numbers in the doubly linked list:");
+            foreach (var number in array)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
