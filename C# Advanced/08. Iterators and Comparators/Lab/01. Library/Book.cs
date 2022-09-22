08bbad0d92d2719da6be7979e6dfe15c6e386f02
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-22 14:53:33 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Lab/01. Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+    }
+}
---
+++ C# Advanced/08. Iterators and Comparators/Lab/01. Library/Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+    }
+}
