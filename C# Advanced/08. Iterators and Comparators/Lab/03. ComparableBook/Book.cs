f0ca6066ccdd7793be77aafcb4c8fe54f736305b
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-22 14:53:52 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Lab/03. ComparableBook/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+
+        public int CompareTo([AllowNull] Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+    }
+}
