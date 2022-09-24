8bbdc18921469ed6ee8bb188c3248f99d2ea1e35
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-24 16:53:22 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Exercises/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+        private int currentIndex;
+        public ListyIterator(params T[] elements)
+        {
+            this.list = new List<T>(elements);
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                this.currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex + 1 <= this.list.Count - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            Console.WriteLine(this.list[currentIndex]);
+        }
+    }
+}
---
+++ C# Advanced/08. Iterators and Comparators/Exercises/01.ListyIterator/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+namespace _01.ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ListyIterator<string> list = new ListyIterator<string>(input.Split().Skip(1).ToArray());
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (input == "Print")
+                {
+                    list.Print();
+                }
+                else if (input == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+                else if (input == "Move")
+                {
+                    Console.WriteLine(list.Move()); 
+                }
+            }
+        }
+    }
+}
