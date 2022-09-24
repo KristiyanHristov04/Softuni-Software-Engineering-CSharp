c245b3d81e8cfd1d9488fbbc1c868e2b6ada1198
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-24 16:53:34 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Exercises/02.Collection/ListyIterator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int currentIndex;
+        public ListyIterator(params T[] elements)
+        {
+            this.collection = new List<T>(elements);
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                this.currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex + 1 <= this.collection.Count - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            Console.WriteLine(this.collection[currentIndex]);
+        }
+        public void PrintAll()
+        {
+            List<T> newList = new List<T>();
+            foreach (T element in this.collection)
+            {
+                newList.Add(element);
+            }
+            Console.WriteLine(String.Join(" ", newList));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in this.collection)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/08. Iterators and Comparators/Exercises/02.Collection/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Collection
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ListyIterator<string> list = new ListyIterator<string>(input.Split().Skip(1).ToArray());
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (input == "Print")
+                {
+                    list.Print();
+                }
+                else if (input == "PrintAll")
+                {
+                    list.PrintAll();
+                }
+                else if (input == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+                else if (input == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                }
+            }
+        }
+    }
+}
