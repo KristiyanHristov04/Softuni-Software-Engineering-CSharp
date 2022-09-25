82db37bb04d0bf44f1e82cb14d5c41aa098c0162
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-25 15:20:32 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Exercises/04. Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] lake;
+        public Lake(params int[] stones)
+        {
+            this.lake = stones;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.lake.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return lake[i];
+                }
+            }
+
+            for (int i = lake.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return lake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/08. Iterators and Comparators/Exercises/04. Froggy/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
