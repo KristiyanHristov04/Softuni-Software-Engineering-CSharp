f3cf094d9515dd3530df9d6b2c1efae1e4296ccb
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-25 15:20:18 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Exercises/03.Stack/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+namespace _03.Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            while (input != "END")
+            {
+                if (input.Contains("Push"))
+                {
+                    string[] elements = input
+                        .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Skip(1).ToArray();
+
+                    foreach (string element in elements)
+                    {
+                        stack.Push(int.Parse(element));
+                    }
+                }
+                else if (input == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var number in stack)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (var number in stack)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
