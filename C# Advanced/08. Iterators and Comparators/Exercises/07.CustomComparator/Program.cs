3e3f1b0d5c22809e53980479c6302e003ea47044
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-26 16:32:42 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Exercises/07.CustomComparator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.CustomComparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(array);
+            var sortedArray = array.OrderBy(number => number % 2 != 0).ThenBy(number => number % 2 == 0);
+            Console.WriteLine(String.Join(" ", sortedArray));
+        }
+    }
+}
