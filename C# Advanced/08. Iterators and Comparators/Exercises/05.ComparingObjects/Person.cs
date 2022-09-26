a423ad5056f988b958ff75236919f8d92a4911f7
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-26 16:32:22 +0300

    .

---
+++ C# Advanced/08. Iterators and Comparators/Exercises/05.ComparingObjects/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private string Town { get; set; }
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+
+            if (result == 0)
+            {
+                result = this.Town.CompareTo(other.Town);
+            }
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/08. Iterators and Comparators/Exercises/05.ComparingObjects/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                string town = tokens[2];
+                Person currPerson = new Person(name, age, town);
+                people.Add(currPerson);
+            }
+            int n = Convert.ToInt32(Console.ReadLine()) - 1;
+            int equal = 0;
+            int notEqual = 0;
+            Person personToCheck = people[n];
+            foreach (var person in people)
+            {
+                if (person.CompareTo(personToCheck) == 0)
+                {
+                    equal++;
+                }
+                else
+                {
+                    notEqual++;
+                }
+            }
+            if (equal <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equal} {notEqual} {people.Count}");
+            }
+        }
+    }
+}
