48d029f73e27ba0e15da1bd0e32489082e77b96d
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-27 16:58:38 +0300

    .

---
+++ C# Advanced/09. Basic Algorithms - Exercise/02. Recursive Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(n)); // 5x4x3x2x1 = 120
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
