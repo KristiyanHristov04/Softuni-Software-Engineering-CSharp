63634e83f129206eda6f678152dc12c437e1a66c
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-29 20:44:45 +0300

    .

---
+++ C# Advanced/09. Basic Algorithms - Exercise/06. Bubble Sort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Bubble_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bubble Sort -> The Bubble Sort algorithm is STABLE!
+            int[] array = new int[] { 20, 42, 14, 37, 4, 33, 50, 10, 36, 19, 40, 13, 24, 34, 49 };
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i + 1 < array.Length && array[i] > array[i + 1])
+                    {
+                        isSorted = false;
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
