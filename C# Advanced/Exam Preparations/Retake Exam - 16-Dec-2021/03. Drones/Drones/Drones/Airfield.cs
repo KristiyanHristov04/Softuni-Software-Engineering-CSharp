7187c5bde6c63c2a4ec3dd8d8191143e27ae7350
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-02 15:12:35 +0300

    .

---
+++ C# Advanced/Exam Preparations/Retake Exam - 16-Dec-2021/03. Drones/Drones/Drones/Airfield.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        public List<Drone> Drones { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public double LandingStrip { get; set; }
+        public int Count { get { return Drones.Count; } }
+
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.LandingStrip = landingStrip;
+            this.Drones = new List<Drone>();
+        }
+
+        public string AddDrone(Drone drone)
+        {
+            if (string.IsNullOrEmpty(drone.Name) || string.IsNullOrEmpty(drone.Brand))
+            {
+                return "Invalid drone.";
+            }
+
+            if (drone.Range < 5 || drone.Range > 15)
+            {
+                return "Invalid drone.";
+            }
+
+            if (Drones.Count + 1 > Capacity)
+            {
+                return "Airfield is full.";
+            }
+
+            this.Drones.Add(drone);
+            return $"Successfully added {drone.Name} to the airfield.";
+        }
+
+        public bool RemoveDrone(string name)
+        {
+            bool doesExist = Drones.Exists(drone => drone.Name == name);
+            if (doesExist)
+            {
+                Drones.RemoveAll(drone => drone.Name == name);
+                return doesExist;
+            }
+            else
+            {
+                return doesExist;
+            }
+        }
+
+        public int RemoveDroneByBrand(string brand)
+        {
+            int count = Drones.Count(drone => drone.Brand == brand);
+            if (count > 0)
+            {
+                Drones.RemoveAll(drone => drone.Brand == brand);
+                return count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Drone FlyDrone(string name)
+        {
+            bool doesExist = Drones.Exists(drone => drone.Name == name);
+            if (doesExist)
+            {
+                foreach (var drone in Drones)
+                {
+                    if (drone.Name == name)
+                    {
+                        drone.Available = false;
+                        return drone;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public List<Drone> FlyDronesByRange(int range)
+        {
+            List<Drone> meetingConditionDrones = Drones.Where(drone => drone.Range >= range).ToList();
+            foreach (var drone in Drones)
+            {
+                if (drone.Range >= range)
+                {
+                    drone.Available = false;
+                }
+            }
+            return meetingConditionDrones;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Drones available at {this.Name}:");
+            List<Drone> notInFlight = Drones.Where(drone => drone.Available == true).ToList();
+            foreach (var drone in notInFlight)
+            {
+                sb.AppendLine(drone.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ C# Advanced/Exam Preparations/Retake Exam - 16-Dec-2021/03. Drones/Drones/Drones/Drone.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Drones
+{
+    public class Drone
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Range { get; set; }
+        public bool Available { get; set; }
+
+        public Drone(string name, string brand, int range)
+        {
+            this.Name = name;
+            this.Brand = brand;
+            this.Range = range;
+            this.Available = true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Drone: {this.Name}");
+            sb.AppendLine($"Manufactured by: {this.Brand}");
+            sb.AppendLine($"Range: {this.Range} kilometers");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
