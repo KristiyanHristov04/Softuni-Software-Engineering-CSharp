9737a25681cac0255bfddd8ee27f096582612b17
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-04 15:31:44 +0300

    .

---
+++ C# Advanced/Exam Preparations/Exam - 22 Feb 2020/03. Guild/Guild/Guild.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        public List<Player> Roster { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.Roster.Count; } }
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Roster = new List<Player>();
+        }
+        public void AddPlayer(Player player)
+        {
+            if (this.Roster.Count + 1 <= this.Capacity)
+            {
+                Roster.Add(player);
+            }
+        }
+        public bool RemovePlayer(string name)
+        {
+            bool doesExist = false;
+            foreach (Player player in this.Roster)
+            {
+                if (player.Name == name)
+                {
+                    this.Roster.Remove(player);
+                    doesExist = true;
+                    return doesExist;
+                }
+            }
+            return doesExist;
+        }
+        public void PromotePlayer(string name)
+        {
+            foreach (var player in this.Roster)
+            {
+                if (player.Name == name && player.Rank != "Member")
+                {
+                    player.Rank = "Member";
+                }
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            foreach (var player in this.Roster)
+            {
+                if (player.Name == name && player.Rank != "Trial")
+                {
+                    player.Rank = "Trial";
+                }
+            }
+        }
+        public Player[] KickPlayersByClass(string class_)
+        {
+            Player[] kickedPlayers = this.Roster.Where(player => player.Class == class_).ToArray();
+            this.Roster.RemoveAll(player => player.Class == class_);
+            return kickedPlayers;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {this.Name}");
+            foreach (var player in this.Roster)
+            {
+                sb.AppendLine(player.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exam Preparations/Exam - 22 Feb 2020/03. Guild/Guild/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+        public Player(string name, string class_)
+        {   
+            this.Name = name;
+            this.Class = class_;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Player {this.Name}: {this.Class}");
+            sb.AppendLine($"Rank: {this.Rank}");
+            sb.AppendLine($"Description: {this.Description}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
