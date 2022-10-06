ad06f1850eb15853c0aaf5852f0886e726df1035
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-06 18:07:46 +0300

    .

---
+++ C# Advanced/Exam Preparations/Exam - 26 June 2021/03. Ski Rental/SkiRental/Ski.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiRental
+{
+    public class Ski
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public Ski(string manufacturer, string model, int year)
+        {
+            this.Manufacturer = manufacturer;   
+            this.Model = model;
+            this.Year = year;
+        }
+        public override string ToString()
+        {
+            return $"{this.Manufacturer} - {this.Model} - {this.Year}";
+        }
+    }
+}
---
+++ C# Advanced/Exam Preparations/Exam - 26 June 2021/03. Ski Rental/SkiRental/SkiRental.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        public List<Ski> Data { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.Data.Count; } }
+        public SkiRental(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Data = new List<Ski>();
+        }
+        public void Add(Ski ski)
+        {
+            if (this.Data.Count + 1 <= this.Capacity)
+            {
+                this.Data.Add(ski);
+            }
+        }
+        public bool Remove(string manufacturer, string model)
+        {
+            bool doesExist = false;
+            foreach (var ski in this.Data)
+            {
+                if (ski.Manufacturer == manufacturer && ski.Model == model)
+                {
+                    doesExist = true;
+                    this.Data.Remove(ski);
+                    return doesExist;
+                }
+            }
+            return false;
+        }
+        public Ski GetNewestSki()
+        {
+            if (this.Data.Count == 0)
+            {
+                return null;
+            }
+            return this.Data.OrderByDescending(ski => ski.Year).First();
+        }
+        public Ski GetSki(string manufacturer, string model)
+        {
+            foreach (var ski in this.Data)
+            {
+                if (ski.Manufacturer == manufacturer && ski.Model == model)
+                {
+                    return ski;
+                }
+            }
+            return null;
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {this.Name}:");
+            foreach (var ski in this.Data)
+            {
+                sb.AppendLine(ski.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
