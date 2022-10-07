390720d1ca4eb0c2edf923ce06496c1ec0a93f75
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-07 20:39:31 +0300

    .

---
+++ C# Advanced/Exam Preparations/Retake Exam - 14 April 2021/01. Warm Winter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Warm_Winter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            List<int> createdSets = new List<int>();
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int currentHat = hats.Peek();
+                int currentScarf = scarfs.Peek();
+                if (currentHat > currentScarf)
+                {
+                    int createSet = currentHat + currentScarf;
+                    hats.Pop();
+                    scarfs.Dequeue();
+                    createdSets.Add(createSet);
+                }
+                else if(currentScarf > currentHat)
+                {
+                    hats.Pop();
+                }
+                else if (currentScarf == currentHat)
+                {
+                    scarfs.Dequeue();
+                    hats.Pop();
+                    hats.Push(currentHat + 1);
+                }
+            }
+            Console.WriteLine($"The most expensive set is: {createdSets.Max()}");
+            Console.WriteLine(String.Join(" ", createdSets));
+        }
+    }
+}
