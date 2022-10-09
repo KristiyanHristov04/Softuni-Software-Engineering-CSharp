95e05c3b97e66e8b68fdda4a7e0cb5ed63921f03
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-09 15:34:31 +0300

    .

---
+++ C# Advanced/Exam Preparations/Retake Exam - 16 December 2020/03. BakeryOpenning/BakeryOpenning/Bakery.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        public List<Employee> Employees { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.Employees.Count; } }
+        public Bakery(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Employees = new List<Employee>();
+        }
+        public void Add(Employee employee)
+        {
+            if (this.Employees.Count + 1 <= this.Capacity)
+            {
+                this.Employees.Add(employee);
+            }
+        }
+        public bool Remove(string name)
+        {
+            bool doesExist = false;
+            foreach (var employee in this.Employees)
+            {
+                if (employee.Name == name)
+                {
+                    doesExist = true;
+                    this.Employees.Remove(employee);
+                    return doesExist;
+                }
+            }
+            return doesExist;
+        }
+        public Employee GetOldestEmployee()
+        {
+            Employee oldestEmployee = this.Employees.OrderByDescending(employee => employee.Age).First();
+            return oldestEmployee;
+        }
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = this.Employees.Where(emp => emp.Name == name).First();
+            return employee;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}:");
+            foreach (var employee in this.Employees)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exam Preparations/Retake Exam - 16 December 2020/03. BakeryOpenning/BakeryOpenning/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public Employee(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employee: {this.Name}, {this.Age} ({this.Country})");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
