a4065469bd4d3c5f90e2b9229a6899c153c19e6a
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-10 18:37:07 +0300

    .

---
+++ C# Advanced/Exam Preparations/Retake Exam - 19 August 2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int createdWreaths = 0;
+            int storedFlowers = 0;
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int currentLilia = lilies.Peek();
+                int currentRoses = roses.Peek();
+                int sum = currentLilia + currentRoses;
+                if (sum == 15)
+                {
+                    createdWreaths++;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+                else if (sum > 15)
+                {
+                    lilies.Pop();
+                    lilies.Push(currentLilia - 2);
+                }
+                else if (sum < 15)
+                {
+                    storedFlowers += sum;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }
+            }
+            while (storedFlowers >= 15)
+            {
+                storedFlowers -= 15;
+                createdWreaths++;
+            }
+
+            if (createdWreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {createdWreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - createdWreaths} wreaths more!");
+            }
+        }
+    }
+}
