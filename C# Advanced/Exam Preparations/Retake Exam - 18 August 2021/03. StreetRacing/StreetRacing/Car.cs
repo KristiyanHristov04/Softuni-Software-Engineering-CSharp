b003d133f6219ebbaedc7556a5143887456894ce
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-12 14:39:52 +0300

    .

---
+++ C# Advanced/Exam Preparations/Retake Exam - 18 August 2021/03. StreetRacing/StreetRacing/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"License Plate: {this.LicensePlate}");
+            sb.AppendLine($"Horse Power: {this.HorsePower}");
+            sb.AppendLine($"Weight: {this.Weight}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exam Preparations/Retake Exam - 18 August 2021/03. StreetRacing/StreetRacing/Race.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public List<Car> Participants { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+        public int Count { get { return this.Participants.Count; } }
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+            this.Participants = new List<Car>();
+        }
+        public void Add(Car car)
+        {
+            bool doesCarWithThisPlateExists = this.Participants.Exists(currCar => currCar.LicensePlate == car.LicensePlate);
+            if (!doesCarWithThisPlateExists && car.HorsePower <= this.MaxHorsePower && this.Participants.Count + 1 <= this.Capacity)
+            {
+                this.Participants.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            bool doesExist = false;
+            foreach (Car car in this.Participants)
+            {
+                if (car.LicensePlate == licensePlate)
+                {
+                    doesExist = true;
+                    this.Participants.Remove(car);
+                    return doesExist;
+                }
+            }
+            return doesExist;
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            foreach (var car in this.Participants)
+            {
+                if (car.LicensePlate == licensePlate)
+                {
+                    return car;
+                }
+            }
+            return null;
+        }
+        public Car GetMostPowerfulCar()
+        {
+            if (this.Participants.Count > 0)
+            {
+                Car mostPowerfulCar = this.Participants.OrderByDescending(car => car.HorsePower).First();
+                return mostPowerfulCar;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {this.Name} - Type: {this.Type} (Laps: {this.Laps})");
+            foreach (var car in this.Participants)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
