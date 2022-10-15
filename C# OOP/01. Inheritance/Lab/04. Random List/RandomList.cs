d5a51259bd3fc947bb2bb57338f1a7e7406ac9d2
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-15 16:43:56 +0300

    .

---
+++ C# OOP/01. Inheritance/Lab/04. Random List/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            if (this.Count == 0)
+            {
+                return null;
+            }
+            Random random = new Random();
+            int index = random.Next(0, this.Count);
+            string removedElement = this[index];
+            this.RemoveAt(index);
+            return removedElement;
+        }
+    }
+}
---
+++ C# OOP/01. Inheritance/Lab/04. Random List/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Ivan1");
+            list.Add("Ivan2");
+            list.Add("Ivan3");
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
