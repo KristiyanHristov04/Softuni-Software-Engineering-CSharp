be1e428a400372022127c0a0b573167532f18466
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-15 16:43:16 +0300

    .

---
+++ C# OOP/01. Inheritance/Lab/01. Single Inheritance/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
