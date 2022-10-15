898eb326f0cf36383809d68aa0310a96658a0540
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-15 16:44:02 +0300

    .

---
+++ C# OOP/01. Inheritance/Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public Stack<string> AddRange(IEnumerable<string> elements)
+        {
+            foreach (var item in elements)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
---
+++ C# OOP/01. Inheritance/Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            stackOfStrings.AddRange(new List<string> { "3", "2", "1" });
+            Console.WriteLine(stackOfStrings.IsEmpty());
+        }
+    }
+}
