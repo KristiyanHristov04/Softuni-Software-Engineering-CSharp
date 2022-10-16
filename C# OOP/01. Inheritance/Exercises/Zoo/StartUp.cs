447ca220b836fd3946385c58ddd6b0cccd06c771
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-16 18:25:05 +0300

    .

---
+++ C# OOP/01. Inheritance/Exercises/Zoo/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Bear bear = new Bear("Mechka");
+            Snake snake = new Snake("Kobra");
+            System.Console.WriteLine(bear.Name);
+            System.Console.WriteLine(snake.Name);
+        }
+    }
+}
