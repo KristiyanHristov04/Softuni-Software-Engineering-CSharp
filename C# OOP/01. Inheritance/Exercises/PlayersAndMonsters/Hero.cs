fe735ff31f3d8b72f9b3cdc9c6a025ac40cd4f25
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-16 18:24:56 +0300

    .

---
+++ C# OOP/01. Inheritance/Exercises/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public string Username { get; set; }
+        public int Level { get; set; }
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
---
+++ C# OOP/01. Inheritance/Exercises/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Wizard wizard = new Wizard("Normal Wizard", 15);
+            DarkWizard darkWizard = new DarkWizard("Dark Wizard", 25);
+            System.Console.WriteLine(wizard);
+            System.Console.WriteLine(darkWizard);
+        }
+    }
+}
