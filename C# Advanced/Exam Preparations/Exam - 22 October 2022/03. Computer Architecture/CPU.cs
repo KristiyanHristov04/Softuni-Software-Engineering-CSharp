3fececb87e1beb0d74274ed5ab36e6ab013dc951
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-22 15:05:36 +0300

    ,.

---
+++ C# Advanced/Exam Preparations/Exam - 22 October 2022/03. Computer Architecture/CPU.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        public string Brand { get; set; }
+        public int Cores { get; set; }
+        public double Frequency { get; set; }
+        public CPU(string brand, int cores, double frequency)
+        {
+            this.Brand = brand;
+            this.Cores = cores;
+            this.Frequency = frequency;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"{this.Brand} CPU:");
+            stringBuilder.AppendLine($"Cores: {this.Cores}");
+            stringBuilder.AppendLine($"Frequency: {this.Frequency:f1} GHz");
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
