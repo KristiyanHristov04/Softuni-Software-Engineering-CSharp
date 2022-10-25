e8ffcb8ab4e05cf4a9c8c23dafa88c43d7081b55
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-25 19:52:31 +0300

    .

---
+++ C# OOP/02. Encapsulation/Exercises/04. Pizza Calories/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04._Pizza_Calories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine().Split(' ');
+                string[] doughInfo = Console.ReadLine().Split(' ');
+
+                Dough dough = new Dough(doughInfo[1], doughInfo[2], double.Parse(doughInfo[3]));
+                string pizzaName = pizzaInfo[1];
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string line = Console.ReadLine();
+
+                while (line != "END")
+                {
+                    string[] toppingInput = line.Split(' ');
+                    Topping topping = new Topping(toppingInput[1], double.Parse(toppingInput[2]));
+
+                    pizza.AddToppings(topping);
+
+                    line = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{pizzaName} - {pizza.GetTotalCalories():f2} Calories.");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
