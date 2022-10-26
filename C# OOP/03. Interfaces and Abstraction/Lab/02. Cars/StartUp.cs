a53da83e96de68db9235a25b653692bb6bb84beb
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-26 21:15:16 +0300

    .

---
+++ C# OOP/03. Interfaces and Abstraction/Lab/02. Cars/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            seat.Start();
+            seat.Stop();
+            Console.WriteLine(tesla.ToString());
+            tesla.Start();
+            tesla.Stop();
+        }
+    }
+}
