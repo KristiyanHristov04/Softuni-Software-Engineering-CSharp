845010ed1c27e4df005e8014cd13d50ea262b40d
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-27 14:45:06 +0300

    .

---
+++ C# OOP/03. Interfaces and Abstraction/Exercises/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
