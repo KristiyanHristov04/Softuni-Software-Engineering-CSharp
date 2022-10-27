d333a6a669165a1a34af021d241fe932da7d0cb9
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-27 14:45:12 +0300

    .

---
+++ C# OOP/03. Interfaces and Abstraction/Exercises/02. Multiple Implementation/IPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IPerson
+    {
+        public string Name { get; }
+        public int Age { get; }
+    }
+}
