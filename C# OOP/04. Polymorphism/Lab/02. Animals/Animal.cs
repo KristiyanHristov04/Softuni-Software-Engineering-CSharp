7fbd940413004278962fb8aa0160a29149513066
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-29 22:43:36 +0300

    .

---
+++ C# OOP/04. Polymorphism/Lab/02. Animals/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string favouriteFood;
+
+        public string FavouriteFood
+        {
+            get { return favouriteFood; }
+            set { favouriteFood = value; }
+        }
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
