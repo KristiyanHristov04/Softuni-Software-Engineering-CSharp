d11ec87f0c2071da9586f8c79aa766232f4b538e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-11-01 22:07:04 +0200

    .

---
+++ C# OOP/05. Exceptions and Error Handling/Lab/01. Square Root/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
