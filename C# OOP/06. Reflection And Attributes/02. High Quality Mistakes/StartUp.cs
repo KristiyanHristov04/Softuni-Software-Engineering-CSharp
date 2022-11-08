29236533877225d86ee6cf36fe9a5d7417d229ab
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-11-08 21:24:00 +0200

    .

---
+++ C# OOP/06. Reflection And Attributes/02. High Quality Mistakes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
