234f9b161a48af5f17b50d8b7fc600721c5bc2cf
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-11-08 21:23:55 +0200

    .

---
+++ C# OOP/06. Reflection And Attributes/01. Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] fieldsToInvestigate)
+        {
+            Type classType = Type.GetType(classToInvestigate);
+
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            StringBuilder sb = new StringBuilder();
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {classType.Name}");
+
+            foreach (FieldInfo field in classFields)
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/06. Reflection And Attributes/01. Stealer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Stealer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
