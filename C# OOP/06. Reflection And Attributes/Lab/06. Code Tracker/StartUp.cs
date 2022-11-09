bef7b849d49f8b4347bfba2a11b4ae71b2f64cdb
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-11-09 19:10:09 +0200

    .

---
+++ C# OOP/06. Reflection And Attributes/Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    [Author("Hector")]
+    internal class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true )]
+    class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ C# OOP/06. Reflection And Attributes/Lab/06. Code Tracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine("{0} is written by {1}", method.Name, attr.Name);
+                    }
+                }
+            }
+        }
+    }
+}
