e91f138fd6516b61d9227e23d1784d2342208fb3
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-11-09 19:10:03 +0200

    .

---
+++ C# OOP/06. Reflection And Attributes/Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    internal class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
