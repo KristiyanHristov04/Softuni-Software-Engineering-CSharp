8d32fa1eae59c4d13a168544d8eddb7a715576b8
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-12-04 17:25:58 +0200

    .

---
+++ JavaScript Advanced/Syntax, Functions and Statements/Lab/03. Largest Number.js
@@ -0,0 +1,6 @@
+function solve(number01, number02, number03){
+    let getMaxNumber = Math.max(number01, number02, number03);
+    console.log(`The largest number is ${getMaxNumber}.`);
+}
+
+solve(-3, -5, -22.5);
