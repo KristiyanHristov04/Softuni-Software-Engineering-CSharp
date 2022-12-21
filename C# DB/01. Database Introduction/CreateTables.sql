a13213360b14b7f854bc5c88f20727ecda10880e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-12-21 21:05:09 +0200

    .

---
+++ C# DB/01. Database Introduction/CreateTables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [Minions](
+		[Id] INT PRIMARY KEY,
+		[Name] NVARCHAR(50),
+		[Age] INT
+)
+
+CREATE TABLE [Towns](
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50)
+)
+
+SELECT * FROM [Minions]
+
+SELECT * FROM [Towns]
