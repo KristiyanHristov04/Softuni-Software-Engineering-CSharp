17bed1f4d9dd84254f260ae9dc117add63641f2f
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-12-21 21:05:00 +0200

    .

---
+++ C# DB/01. Database Introduction/CreateSoftUniDatabase.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE [SoftUni]
+
+USE [SoftUni]
+
+CREATE TABLE [Towns](
+	[Id] INT PRIMARY KEY IDENTITY(1, 1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Addresses](
+	[Id] INT PRIMARY KEY IDENTITY(1, 1),
+	[AdressText] NVARCHAR(50) NOT NULL,
+	[TownId] INT FOREIGN KEY REFERENCES [Towns]([Id])
+)
+
+CREATE TABLE [Departments](
+	[Id] INT PRIMARY KEY IDENTITY(1, 1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Employees](
+	[Id] INT PRIMARY KEY IDENTITY(1, 1),
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[MiddleName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[JobTitle] NVARCHAR(50) DEFAULT('Programmer'),
+	[DepartmentId] INT FOREIGN KEY REFERENCES [Departments]([Id]),
+	[HireDate] DATE,
+	[Salary] DECIMAL(5, 2) DEFAULT(1260.50),
+	[AddressId] INT FOREIGN KEY REFERENCES [Addresses]([Id])
+)
+
