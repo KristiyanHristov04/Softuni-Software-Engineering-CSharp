5bebc610326397f839e1bd7e6ea9d00d54becd08
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-01 17:48:02 +0200

    .

---
+++ C# DB/03. Table Relations/One-To-ManyRelationship.sql
@@ -0,0 +1,30 @@
+--USE [TRDB]
+
+CREATE TABLE [Manufacturers](
+	[ManufacturerID] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(20) NOT NULL,
+	[EstablishedOn] VARCHAR(10) NOT NULL
+)
+
+INSERT INTO [Manufacturers]([Name], [EstablishedOn])
+VALUES
+	('BMW', '07/03/1916'),
+	('Tesla', '01/01/2003'),
+	('Lada', '01/05/1966')
+
+CREATE TABLE [Models](
+	[ModelID] INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(20) NOT NULL,
+	[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID])
+)
+
+INSERT INTO [Models]([Name], [ManufacturerID])
+VALUES
+	('X1', 1),
+	('i6', 1),
+	('Model S', 2),
+	('Model X', 2),
+	('Model 3', 2),
+	('Nova', 3)
+
+--SELECT * FROM [Models]
