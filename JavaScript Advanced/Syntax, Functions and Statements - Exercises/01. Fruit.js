becfc2e34f55578af00b5eb803bd3a16ee9faef7
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-05 15:59:05 +0200

    .

--- JavaScript Advanced/Syntax, Functions and Statements - Exercises/01. Fruit.js
+++
@@ -1,7 +0,0 @@
-function solve(fruit, weight, price){
-    let fruitWeightInKilograms = Number(weight) / 1000;
-    let neededMoney = fruitWeightInKilograms * Number(price);
-    console.log(`I need $${neededMoney.toFixed(2)} to buy ${fruitWeightInKilograms.toFixed(2)} kilograms ${fruit}.`);
-}
-
-solve('orange', 2500, 1.80);
--- JavaScript Advanced/Syntax, Functions and Statements - Exercises/03. Largest Number.js
+++
@@ -1,6 +0,0 @@
-function solve(number01, number02, number03){
-    let getMaxNumber = Math.max(number01, number02, number03);
-    console.log(`The largest number is ${getMaxNumber}.`);
-}
-
-solve(-3, -5, -22.5);
