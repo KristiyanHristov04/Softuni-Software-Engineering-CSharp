84bdad2b2bf8a448363c17c68bc8b0c92dfa1166
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-08 12:30:45 +0200

    .

---
+++ C# DB/04. Built-in Functions/FindAllEmployeeswithRank2.sql
@@ -0,0 +1,10 @@
+SELECT * FROM
+	(
+		SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+		DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+		FROM [Employees]
+		WHERE [Salary] BETWEEN 10000 AND 50000
+	)
+	AS [RankSubquery]
+	WHERE [Rank] = 2
+	ORDER BY [Salary] DESC
