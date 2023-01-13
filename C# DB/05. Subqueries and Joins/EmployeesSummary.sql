24ff86c354f5fedef57d1d05000714a9295516e4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-13 16:17:13 +0200

    .

---
+++ C# DB/05. Subqueries and Joins/EmployeesSummary.sql
@@ -0,0 +1,12 @@
+SELECT TOP (50)
+[e].EmployeeID,
+CONCAT([e].[FirstName], ' ', [e].[LastName]) AS [EmployeeName],
+CONCAT([e2].[FirstName], ' ', [e2].[LastName]) AS [ManagerName],
+[d].[Name] AS [DepartmentName]
+FROM [Employees] AS [e]
+JOIN [Departments] AS [d] ON [d].DepartmentID = [e].DepartmentID
+JOIN [Employees] AS [e2] ON  [e2].EmployeeID = [e].ManagerID
+ORDER BY [e].EmployeeID
+
+SELECT * FROM [Employees]
+SELECT * FROM [Departments]
