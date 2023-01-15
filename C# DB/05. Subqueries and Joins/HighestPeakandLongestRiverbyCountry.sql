489bc1e48493998b3eebd2164c4a87d6f8ef22d7
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-15 11:43:22 +0200

    .

---
+++ C# DB/05. Subqueries and Joins/HighestPeakandLongestRiverbyCountry.sql
@@ -0,0 +1,16 @@
+SELECT TOP (5) [c].[CountryName],
+               MAX([p].[Elevation]) AS [HighestPeakElevation],
+               MAX([r].[Length]) AS [LongestRiverLength]
+          FROM [Countries] AS [c]
+     LEFT JOIN [CountriesRivers] AS [cr]
+            ON [c].[CountryCode] = [cr].[CountryCode]
+     LEFT JOIN [Rivers] AS [r]
+            ON [cr].[RiverId] = [r].[Id]
+     LEFT JOIN [MountainsCountries] AS [mc]
+            ON [mc].[CountryCode] = [c].[CountryCode]
+     LEFT JOIN [Mountains] AS [m]
+            ON [mc].[MountainId] = [m].[Id]
+     LEFT JOIN [Peaks] AS [p]
+            ON [p].[MountainId] = [m].[Id]
+      GROUP BY [c].[CountryName]
+      ORDER BY [HighestPeakElevation] DESC, [LongestRiverLength] DESC, [CountryName]
