c3cd0d4b38351904be9f4e0e058ffbaf98b1f317
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-16 20:31:55 +0200

    .

---
+++ C# DB/06. Indices and Data Aggregation/AverageInterest.sql
@@ -0,0 +1,9 @@
+SELECT * FROM [WizzardDeposits]
+
+SELECT [DepositGroup], 
+[IsDepositExpired], 
+AVG([DepositInterest]) AS [AverageInterest]
+FROM [WizzardDeposits]
+WHERE [DepositStartDate] > '01/01/1985'
+GROUP BY [DepositGroup], [IsDepositExpired]
+ORDER BY [DepositGroup] DESC, [IsDepositExpired]
