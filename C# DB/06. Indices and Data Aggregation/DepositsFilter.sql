c3d035e49b8d7e70d51c1c20af47ab9de43d5f51
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-16 20:32:04 +0200

    .

---
+++ C# DB/06. Indices and Data Aggregation/DepositsFilter.sql
@@ -0,0 +1,7 @@
+SELECT [DepositGroup], 
+SUM([DepositAmount]) AS [TotalSum] 
+FROM [WizzardDeposits]
+WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [DepositGroup]
+HAVING SUM([DepositAmount]) < 150000
+ORDER BY SUM([DepositAmount]) DESC
