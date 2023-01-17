98f1aa995853467c9b8c6e07f8d93be922b77595
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-17 15:06:06 +0200

    .

---
+++ C# DB/06. Indices and Data Aggregation/DepositsSumforOllivanderFamily.sql
@@ -0,0 +1,7 @@
+SELECT [DepositGroup], 
+SUM([DepositAmount]) AS [TotalSum] 
+FROM [WizzardDeposits]
+WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [DepositGroup]
+
+SELECT * FROM [WizzardDeposits]
