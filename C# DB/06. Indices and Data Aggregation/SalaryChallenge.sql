dcf26c47e045758f130acff26d2162b8b4f0f291
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-01-18 12:05:28 +0200

    .

---
+++ C# DB/06. Indices and Data Aggregation/SalaryChallenge.sql
@@ -0,0 +1,8 @@
+SELECT TOP(10) [FirstTable].[FirstName], [FirstTable].[LastName], [FirstTable].[DepartmentID]
+FROM [Employees][FirstTable]
+LEFT JOIN (SELECT [DepartmentID], AVG([Salary])[AvgSalary]
+					  FROM [Employees] 
+					  GROUP BY [DepartmentID])[SecondTable]
+		  ON [FirstTable].[DepartmentID] = [SecondTable].[DepartmentID]
+WHERE [Salary] > [AvgSalary]
+ORDER BY [FirstTable].[DepartmentID]
