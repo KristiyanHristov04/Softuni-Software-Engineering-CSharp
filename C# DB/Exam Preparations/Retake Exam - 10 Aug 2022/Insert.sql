1b919b0b42dc794c5c32aa42d73a26180aa05b71
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-02-04 09:56:15 +0200

    .

---
+++ C# DB/Exam Preparations/Retake Exam - 10 Aug 2022/Insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO [Tourists]([Name], [Age], [PhoneNumber], [Nationality], [Reward])
+	VALUES
+		('Borislava Kazakova',	52,	'+359896354244',	'Bulgaria',	NULL),
+		('Peter Bosh',	48,	'+447911844141',	'UK',	NULL),
+		('Martin Smith',	29,	'+353863818592',	'Ireland',	'Bronze badge'),
+		('Svilen Dobrev',	49,	'+359986584786',	'Bulgaria',	'Silver badge'),
+		('Kremena Popova',	38	,'+359893298604',	'Bulgaria',	NULL)
+
+INSERT INTO [Sites]([Name], [LocationId], [CategoryId], [Establishment])
+	VALUES
+		('Ustra fortress',	90,	7,	'X'),
+		('Karlanovo Pyramids',	65,	7,	NULL),
+		('The Tomb of Tsar Sevt',	63,	8,	'V BC'),
+		('Sinite Kamani Natural Park',	17,	1,	NULL),
+		('St. Petka of Bulgaria – Rupite',	92,	6,	'1994')
