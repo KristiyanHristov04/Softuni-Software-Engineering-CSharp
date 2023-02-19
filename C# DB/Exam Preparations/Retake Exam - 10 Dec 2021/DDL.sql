e741921aab0a0f53ca4150486222b1c539983b99
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-02-19 10:39:46 +0200

    .

---
+++ C# DB/Exam Preparations/Retake Exam - 10 Dec 2021/DDL.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE [Airport]
+
+GO
+
+USE [Airport]
+
+GO
+
+CREATE TABLE [Passengers](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[FullName] VARCHAR(100) NOT NULL,
+	[Email] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Pilots](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[FirstName] VARCHAR(30) NOT NULL,
+	[LastName] VARCHAR(30) NOT NULL,
+	[Age] TINYINT NOT NULL CHECK([Age] BETWEEN 21 AND 62),
+	[Rating] FLOAT CHECK([Rating] BETWEEN 0.0 AND 10.0)
+)
+
+CREATE TABLE [AircraftTypes](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[TypeName] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE [Aircraft](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[Manufacturer] VARCHAR(25) NOT NULL,
+	[Model] VARCHAR(30) NOT NULL,
+	[Year] INT NOT NULL,
+	[FlightHours] INT,
+	[Condition] CHAR(1) NOT NULL,
+	[TypeId] INT FOREIGN KEY REFERENCES [AircraftTypes]([Id]) NOT NULL
+)
+
+CREATE TABLE [PilotsAircraft](
+	[AircraftId] INT FOREIGN KEY REFERENCES [Aircraft]([Id]) NOT NULL,
+	[PilotId] INT FOREIGN KEY REFERENCES [Pilots]([Id]) NOT NULL,
+	PRIMARY KEY ([AircraftId], [PilotId])
+)
+
+CREATE TABLE [Airports](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[AirportName] VARCHAR(70) NOT NULL,
+	[Country] VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE [FlightDestinations](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[AirportId] INT FOREIGN KEY REFERENCES [Airports]([Id]) NOT NULL,
+	[Start] DATETIME NOT NULL,
+	[AircraftId] INT FOREIGN KEY REFERENCES [Aircraft]([Id]) NOT NULL,
+	[PassengerId] INT FOREIGN KEY REFERENCES [Passengers]([Id]) NOT NULL,
+	[TicketPrice] DECIMAL(18, 2) NOT NULL DEFAULT(15)
+)
