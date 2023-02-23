8b1b42add0cdd15d7c60b555b9a56a663d1ca931
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-02-23 13:37:13 +0200

    .

---
+++ C# DB/Exam/Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO [Boardgames]([Name], [YearPublished], [Rating], [CategoryId], [PublisherId], [PlayersRangeId])
+	VALUES
+		('Deep Blue',	2019,	5.67,	1,	15,	7),
+		('Paris',	2016,	9.78,	7,	1,	5),
+		('Catan: Starfarers',	2021,	9.87,	7,	13,	6),
+		('Bleeding Kansas',	2020,	3.25,	3,	7,	4),
+		('One Small Step',	2019,	5.75,	5,	9,	2)
+
+INSERT INTO [Publishers]([Name], [AddressId], [Website], [Phone])
+	VALUES
+		('Agman Games',	5,	'www.agmangames.com',	'+16546135542'),
+		('Amethyst Games',	7,	'www.amethystgames.com',	'+15558889992'),
+		('BattleBooks',	13,	'www.battlebooks.com',	'+12345678907')
