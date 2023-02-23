dcce075271f7a670ce9da5449c9622ebb2221a62
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-02-23 13:36:57 +0200

    .

---
+++ C# DB/Exam/DDL.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE [Boardgames]
+
+GO
+
+USE [Boardgames]
+
+GO
+
+CREATE TABLE [Categories](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Addresses](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[StreetName] NVARCHAR(100) NOT NULL,
+	[StreetNumber] INT NOT NULL,
+	[Town] VARCHAR(30) NOT NULL,
+	[Country] VARCHAR(50) NOT NULL,
+	[ZIP] INT NOT NULL
+)
+
+CREATE TABLE [Publishers](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL UNIQUE,
+	[AddressId] INT FOREIGN KEY REFERENCES [Addresses]([Id]) NOT NULL,
+	[Website] NVARCHAR(40),
+	[Phone] NVARCHAR(20)
+)
+
+CREATE TABLE [PlayersRanges](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[PlayersMin] INT NOT NULL,
+	[PlayersMax] INT NOT NULL
+)
+
+CREATE TABLE [Boardgames](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(30) NOT NULL,
+	[YearPublished] INT NOT NULL,
+	[Rating] DECIMAL(18,2) NOT NULL,
+	[CategoryId] INT FOREIGN KEY REFERENCES [Categories]([Id]) NOT NULL,
+	[PublisherId] INT FOREIGN KEY REFERENCES [Publishers]([Id]) NOT NULL,
+	[PlayersRangeId] INT FOREIGN KEY REFERENCES [PlayersRanges]([Id]) NOT NULL
+)
+
+CREATE TABLE [Creators](
+	[Id] INT PRIMARY KEY IDENTITY,
+	[FirstName] NVARCHAR(30) NOT NULL,
+	[LastName] NVARCHAR(30) NOT NULL,
+	[Email] NVARCHAR(30) NOT NULL
+)
+
+CREATE TABLE [CreatorsBoardgames](
+	[CreatorId] INT FOREIGN KEY REFERENCES [Creators]([Id]) NOT NULL,
+	[BoardgameId] INT FOREIGN KEY REFERENCES [Boardgames]([Id]) NOT NULL,
+	PRIMARY KEY ([CreatorId], [BoardgameId])
+)
