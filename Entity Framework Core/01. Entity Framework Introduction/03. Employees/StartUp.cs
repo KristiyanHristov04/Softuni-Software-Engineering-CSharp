1bb2fd6e47dd2aca44ee145c89ccb0e622be7448
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-05 11:15:51 +0200

    .

---
+++ Entity Framework Core/01. Entity Framework Introduction/03. Employees/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            using (SoftUniContext dbContext = new SoftUniContext())
+            {
+                string result = GetEmployeesFullInformation(dbContext);
+                Console.WriteLine(result);
+            }
+            
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                output.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
