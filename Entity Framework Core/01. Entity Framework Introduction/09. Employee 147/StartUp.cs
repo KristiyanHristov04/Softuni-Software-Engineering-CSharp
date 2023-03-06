6f0581b6a17a8acd7bc6dfca957cca322c164715
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-06 21:28:35 +0200

    .

---
+++ Entity Framework Core/01. Entity Framework Introduction/09. Employee 147/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (SoftUniContext dbContext = new SoftUniContext())
+            {
+                string result = GetEmployee147(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {           
+            StringBuilder output = new StringBuilder();
+
+            Employee employee = context.Employees
+                .First(e => e.EmployeeId == 147);
+
+
+            var employeeProjects = context.Projects
+                .Where(p => p.EmployeesProjects.Any(ep => ep.EmployeeId == employee.EmployeeId))
+                .Select(p => new
+                {
+                    p.Name
+                });
+
+
+            output.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+            foreach (var project in employeeProjects.OrderBy(empProject => empProject.Name))
+            {
+                output.AppendLine(project.Name);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
