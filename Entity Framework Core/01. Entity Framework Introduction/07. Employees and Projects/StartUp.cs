75845e0bafee50f5da58fd47b5a069d6953c3fd1
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-06 21:26:35 +0200

    .

---
+++ Entity Framework Core/01. Entity Framework Introduction/07. Employees and Projects/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using SoftUni.Data;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+            string output = GetEmployeesInPeriod(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            var EmployeeInfo = context.Employees
+                .Take(10)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    ManagerFirstName = e.Manager.FirstName,
+                    ManagerLastName = e.Manager.LastName,
+                    Projects = e.EmployeesProjects.Where(ep => ep.Project.StartDate.Year >= 2001 & ep.Project.StartDate.Year <= 2003)
+                        .Select(ep => new
+                        {
+                            ProjectName = ep.Project.Name,
+                            StartDate = ep.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                            EndDate = ep.Project.EndDate != null
+                                ? ep.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                                : "not finished"
+                        })
+                })
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var e in EmployeeInfo)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+
+                if (e.Projects.Any())
+                {
+                    sb.AppendLine(String.Join(Environment.NewLine, e.Projects
+                        .Select(p => $"--{p.ProjectName} - {p.StartDate} - {p.EndDate}")));
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Entity Framework Core/01. Entity Framework Introduction/08. Addresses by Town/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (SoftUniContext dbContext = new SoftUniContext())
+            {
+                string result = GetAddressesByTown(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+            var addresses = context.Addresses
+                .OrderByDescending(a => a.Employees.Count())
+                .ThenBy(a => a.Town.Name)
+                .ThenBy(a => a.AddressText)
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeesCount = a.Employees.Count()
+                })
+                .Take(10)
+                .ToArray();
+
+            foreach (var address in addresses)
+            {
+                output.AppendLine($"{address.AddressText}, {address.TownName} - {address.EmployeesCount} employees");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
