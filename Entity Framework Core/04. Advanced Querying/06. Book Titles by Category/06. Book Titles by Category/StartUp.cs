723c88b14b5175b92668aaeb4de4edb9a9d1d56a
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-10 11:07:00 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/06. Book Titles by Category/06. Book Titles by Category/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using BookShop.Data;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string input = Console.ReadLine();
+                string result = GetBooksByCategory(db, input);
+                Console.WriteLine(result);
+                //Console.WriteLine(result.Length);
+            }
+        }
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            StringBuilder output = new StringBuilder();
+            var allBooks = context.Books
+                .Select(b => new
+                {
+                    b.Title,
+                    Category = b.BookCategories.Select(bc => new
+                    {
+                        CategoryName = bc.Category.Name
+                    }),
+                })
+                .OrderBy(b => b.Title)
+                .ToArray();
+
+            foreach (var book in allBooks)
+            {
+                foreach (var category in categories)
+                {
+                    if (book.Category.Any(c => c.CategoryName.ToLower() == category.ToLower()))
+                    {
+                        output.AppendLine(book.Title);
+                    }
+                }
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
