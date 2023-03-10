1966f11d9edd96cfc65269565b1b489a2a913198
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-10 11:04:34 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/02. Age Restriction/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Text;
+
+namespace BookShop 
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string input = Console.ReadLine();
+                string result = GetBooksByAgeRestriction(db, input);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder output = new StringBuilder();
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+            var booksWithRestriction = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            foreach (var book in booksWithRestriction)
+            {
+                output.AppendLine(book);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
