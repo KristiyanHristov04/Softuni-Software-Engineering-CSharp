9fb23a73384b06f6e8cb9b68f97e4594acc4cfc4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-10 11:05:56 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/04. Books by Price/04. Books by Price/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using BookShop.Data;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string result = GetBooksByPrice(db);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            StringBuilder output = new StringBuilder();
+            var allBooks = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .OrderByDescending(b => b.Price)
+                .ToArray();
+
+            foreach (var book in allBooks)
+            {
+                output.AppendLine($"{book.Title} - ${book.Price:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
