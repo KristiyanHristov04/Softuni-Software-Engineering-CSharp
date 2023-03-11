964a3ea4030642c155e4f9ffe7461790a96666ae
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-11 22:39:23 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/10. Book Search by Author/10. Book Search by Author/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using BookShop.Data;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string input = Console.ReadLine();
+                string result = GetBooksByAuthor(db, input);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var booksAndAuthors = context.Books
+                .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                .OrderBy(b => b.BookId)
+                .Select(b => new
+                {
+                    b.Title,
+                    AuthorFullName = $"{b.Author.FirstName} {b.Author.LastName}"
+                })
+                .ToArray();
+
+            foreach (var book in booksAndAuthors)
+            {
+                output.AppendLine($"{book.Title} ({book.AuthorFullName})");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
