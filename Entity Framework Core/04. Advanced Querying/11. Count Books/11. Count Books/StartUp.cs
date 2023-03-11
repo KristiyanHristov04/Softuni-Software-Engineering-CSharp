78eee880851f2f8f75260b7ef5a4fcafa0d9b240
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-11 22:39:40 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/11. Count Books/11. Count Books/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using BookShop.Data;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                int bookTitleLength = Convert.ToInt32(Console.ReadLine());
+                int result = CountBooks(db, bookTitleLength);
+                Console.WriteLine(result);
+            }
+        }
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int count = context.Books
+                .Where(b => b.Title.Length > lengthCheck)
+                .Count();
+
+            return count;
+        }
+    }
+}
