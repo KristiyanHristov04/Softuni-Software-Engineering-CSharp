370dfd264508717ebb2d62f5fedf7c2c0f865192
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-11 22:37:48 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/07. Released Before Date/07. Released Before Date/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using BookShop.Data;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string date = Console.ReadLine();
+                string result = GetBooksReleasedBefore(db, date);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            StringBuilder output = new StringBuilder();
+            string[] yearSplitted = date.Split('-', StringSplitOptions.RemoveEmptyEntries);
+            int day = Convert.ToInt32(yearSplitted[0]);
+            int month = Convert.ToInt32(yearSplitted[1]);
+            int year = Convert.ToInt32(yearSplitted[2]);
+            DateTime givenDateTime = new DateTime(year, month, day);
+
+            var allBooks = context.Books
+                .Where(b => b.ReleaseDate < givenDateTime)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.EditionType,
+                    b.Price
+                })
+                .ToArray();
+
+            foreach (var book in allBooks)
+            {
+                output.AppendLine($"{book.Title} - {book.EditionType.ToString()} - ${book.Price:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
