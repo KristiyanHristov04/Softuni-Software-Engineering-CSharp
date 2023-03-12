318e6ad7b0c4b170a2c3ed43ac5061200159a154
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-12 16:17:49 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/14. Most Recent Books/14. Most Recent Books/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using BookShop.Data;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        { 
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string result = GetMostRecentBooks(db);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            var categoriesWithBooks = context.Categories
+                .Select(c => new
+                {
+                    CategoryName = c.Name,
+                    CategoryBooks = c.CategoryBooks.Select(cb => new
+                    {
+                        cb.Book
+                    })
+                })
+                .OrderBy(c => c.CategoryName)
+                .ToArray();
+
+            foreach (var category in categoriesWithBooks)
+            {
+                output.AppendLine($"--{category.CategoryName}");
+                int count = 1;
+                foreach (var book in category.CategoryBooks.OrderByDescending(cb => cb.Book.ReleaseDate))
+                {
+                    if (count == 4)
+                    {
+                        break;
+                    }
+                    output.AppendLine($"{book.Book.Title} ({book.Book.ReleaseDate.Value.Year})");
+                    count++;
+                }
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
