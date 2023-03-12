492dcd02dc576a4412c24d4f9e02ad5cdfceb4ca
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-12 16:17:05 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/16. Remove Books/16. Remove Books/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using BookShop.Data;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                int result = RemoveBooks(db);
+                Console.WriteLine(result);
+            }
+        }
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var booksToBeRemoved = context.Books
+                .Where(b => b.Copies < 4200)
+                .ToArray();
+
+            context.Books.RemoveRange(booksToBeRemoved);
+
+            context.SaveChanges();
+
+            int countOfRemovedBooks = booksToBeRemoved.Count();
+
+            return countOfRemovedBooks;
+        }
+    }
+}
