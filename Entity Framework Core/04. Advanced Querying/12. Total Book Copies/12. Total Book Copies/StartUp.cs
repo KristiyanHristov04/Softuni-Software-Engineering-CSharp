0a2d4291a70cbbcf0e6d29f15fb970097a1885e4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-12 16:15:41 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/12. Total Book Copies/12. Total Book Copies/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using BookShop.Data;
+using BookShop.Models;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+                string result = CountCopiesByAuthor(db);
+                Console.WriteLine(result);
+            }
+        }
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            StringBuilder output = new StringBuilder();
+            var authors = context.Authors
+                .Select(a => new
+                {
+                    FullName = $"{a.FirstName} {a.LastName}",
+                    TotalBookCopies = a.Books.Select(b => b.Copies).Sum()
+                })
+                .OrderByDescending(a => a.TotalBookCopies)
+                .ToArray();
+
+               
+            foreach (var author in authors)
+            {
+                output.AppendLine($"{author.FullName} - {author.TotalBookCopies}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
