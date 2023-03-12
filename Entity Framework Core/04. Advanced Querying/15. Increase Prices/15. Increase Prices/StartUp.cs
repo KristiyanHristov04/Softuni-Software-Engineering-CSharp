a88e6c2747586f283a9da1ea9cdffcd9b8e2f1c2
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-12 16:16:37 +0200

    .

---
+++ Entity Framework Core/04. Advanced Querying/15. Increase Prices/15. Increase Prices/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using BookShop.Data;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (BookShopContext db = new BookShopContext())
+            {
+                IncreasePrices(db);
+            }
+        }
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year < 2010)
+                .ToArray();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
