50ada293368b2703e3a9e6f1ba2c53d3c517ab7e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-14 15:43:26 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/02. Import Products/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (ProductShopContext dbContext = new ProductShopContext())
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                string inputJsonUsers = File.ReadAllText(@"../../../Datasets/users.json");
+                ImportUsers(dbContext, inputJsonUsers);
+
+                string readJson = File.ReadAllText(@"../../../Datasets/products.json");
+                string result = ImportProducts(dbContext, readJson);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count()}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+            context.Products.AddRange(products);
+            context.SaveChanges();
+            return $"Successfully imported {products.Count}";
+        }
+    }
+}
