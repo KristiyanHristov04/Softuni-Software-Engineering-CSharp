0cb6fbbf102e38e9a7c54e9b506b20294db3a6cf
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-14 15:44:15 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/05. Export Products In Range/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (ProductShopContext dbContext = new ProductShopContext())
+            {
+                string result = GetProductsInRange(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .Select(p => new
+                {
+                    name = p.Name,
+                    price = p.Price,
+                    seller = $"{p.Seller.FirstName} {p.Seller.LastName}"
+                })
+                .ToArray();
+
+            string productsToJson = JsonConvert.SerializeObject(products, Formatting.Indented);
+            return productsToJson;
+        }
+    }
+}
