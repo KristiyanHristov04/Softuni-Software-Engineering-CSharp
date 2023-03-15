b481e0426bc08bb03e2740c3025fe212139e44a4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-15 18:13:05 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/06. Export Sold Products/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (ProductShopContext dbContext = new ProductShopContext())
+            {
+                string result = GetSoldProducts(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Any(sp => sp.Buyer != null))
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    soldProducts = u.ProductsSold.Select(ps => new
+                    {
+                        name = ps.Name,
+                        price = ps.Price,
+                        buyerFirstName = ps.Buyer.FirstName,
+                        buyerLastName = ps.Buyer.LastName,
+                    })
+                })
+                .ToArray();
+
+            string usersToJson = JsonConvert.SerializeObject(users, Formatting.Indented);
+            return usersToJson;
+        }
+    }
+}
