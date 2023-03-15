e273b131ba4d98e1fc10220ae8a4352a3235ab1d
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-15 18:14:30 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/10. Import Parts/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string jsonPath = File.ReadAllText(@"../../../Datasets/parts.json");
+                string result = ImportParts(dbContext, jsonPath);
+                Console.WriteLine(result);
+            }
+        }
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            Part[] parts = JsonConvert.DeserializeObject<Part[]>(inputJson)
+                .Where(p => p.SupplierId <= 31)
+                .ToArray();
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+            return $"Successfully imported {parts.Count()}.";
+        }
+    }
+}
