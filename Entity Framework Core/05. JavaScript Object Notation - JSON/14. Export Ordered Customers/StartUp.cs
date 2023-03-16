b384aba98d36df9e688feb16cf9e92328966fe54
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-16 21:14:11 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/14. Export Ordered Customers/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetOrderedCustomers(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .OrderBy(c => c.BirthDate)
+                .ThenBy(c => c.IsYoungDriver)
+                .Select(c => new
+                {
+                    c.Name,
+                    BirthDate = c.BirthDate.ToString("dd/MM/yyyy"),
+                    c.IsYoungDriver
+                })
+                .ToArray();
+
+            string customersToJson = JsonConvert.SerializeObject(customers, Formatting.Indented);
+            return customersToJson;
+        }
+    }
+}
