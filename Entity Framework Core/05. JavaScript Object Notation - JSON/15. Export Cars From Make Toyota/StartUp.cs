985a6bf4d5943b705b06698ac80e28270d38157c
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-16 21:14:16 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/15. Export Cars From Make Toyota/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetCarsFromMakeToyota(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Where(c => c.Make == "Toyota")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .Select(c => new
+                {
+                    c.Id,
+                    c.Make,
+                    c.Model,
+                    c.TraveledDistance
+                })
+                .ToArray();
+
+            string carsToJson = JsonConvert.SerializeObject(cars, Formatting.Indented);
+            return carsToJson;
+        }
+    }
+}
