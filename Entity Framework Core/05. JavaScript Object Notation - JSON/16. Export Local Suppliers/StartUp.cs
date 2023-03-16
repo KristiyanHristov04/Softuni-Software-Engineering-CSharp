d0ad933fa15d6248b4c413a28a74cbe27a9e9fbd
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-16 21:14:21 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/16. Export Local Suppliers/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetLocalSuppliers(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var suppliers = context.Suppliers
+                .Where(s => s.IsImporter == false)
+                .Select(s => new
+                {
+                    s.Id,
+                    s.Name,
+                    PartsCount = s.Parts.Count()
+                })
+                .ToArray();
+
+            string suppliersToJson = JsonConvert.SerializeObject(suppliers, Formatting.Indented);
+            return suppliersToJson;
+        }
+    }
+}
