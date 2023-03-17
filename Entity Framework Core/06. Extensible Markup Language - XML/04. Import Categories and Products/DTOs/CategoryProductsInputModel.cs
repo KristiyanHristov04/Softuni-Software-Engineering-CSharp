79a3fe8b35db05c94d0beb74b0dd1b96cd6b8d7b
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-17 15:22:07 +0200

    .

---
+++ Entity Framework Core/06. Extensible Markup Language - XML/04. Import Categories and Products/DTOs/CategoryProductsInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs
+{
+    [XmlType("CategoryProduct")]
+    public class CategoryProductsInputModel
+    {
+        [XmlElement("CategoryId")]
+        public int CategoryId { get; set; }
+
+        [XmlElement("ProductId")]
+        public int ProductId { get; set; }
+    }
+}
