06b3c041fb58330b7e84b84945e464d91ee0a5b8
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-19 13:24:39 +0200

    .

---
+++ Entity Framework Core/06. Extensible Markup Language - XML/14. Export Cars With Distance/DTOs/CarOutputModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs
+{
+    [XmlType("car")]
+    public class CarOutputModel
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("traveled-distance")]
+        public long TravelledDistance { get; set; }
+    }
+}
---
+++ Entity Framework Core/06. Extensible Markup Language - XML/14. Export Cars With Distance/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetCarsWithDistance(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            CarOutputModel[] carDtos = context.Cars
+                .Where(c => c.TraveledDistance > 2000000)
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .Select(c => new CarOutputModel()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TravelledDistance = c.TraveledDistance
+                })
+                .Take(10)
+                .ToArray();
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("cars");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(CarOutputModel[]), xmlRoot);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, carDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
