8199b3a2d95f15070b7f8d6bb8434677c925cd30
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-20 20:33:12 +0200

    .

---
+++ Entity Framework Core/06. Extensible Markup Language - XML/18. Export Total Sales By Customer/DTOs/CustomerOutputModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs
+{
+    [XmlType("customer")]
+    public class CustomerOutputModel
+    {
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public string SpentMoney { get; set; }
+    }
+}
---
+++ Entity Framework Core/06. Extensible Markup Language - XML/18. Export Total Sales By Customer/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetTotalSalesByCustomer(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var tempDto = context.Customers
+                .Where(c => c.Sales.Any())
+                .Select(c => new
+                {
+                    FullName = c.Name,
+                    BoughtCars = c.Sales.Count(),
+                    SalesInfo = c.Sales.Select(s => new
+                    {
+                        Prices = c.IsYoungDriver
+                            ? s.Car.PartsCars.Sum(p => Math.Round((double)p.Part.Price * 0.95, 2))
+                            : s.Car.PartsCars.Sum(p => (double)p.Part.Price)
+                    }).ToArray(),
+                })
+                .ToArray();
+
+            CustomerOutputModel[] totalSalesDtos = tempDto
+                .OrderByDescending(t => t.SalesInfo.Sum(s => s.Prices))
+                .Select(t => new CustomerOutputModel()
+                {
+                    FullName = t.FullName,
+                    BoughtCars = t.BoughtCars,
+                    SpentMoney = t.SalesInfo.Sum(s => s.Prices).ToString("f2")
+                })
+                .ToArray();
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("customers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(CustomerOutputModel[]), xmlRoot);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, totalSalesDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
