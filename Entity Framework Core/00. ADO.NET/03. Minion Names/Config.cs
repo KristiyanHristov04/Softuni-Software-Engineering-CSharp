6a99af7578847d00673c9d252d2336b2b6c0b910
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-21 16:15:53 +0200

    .

---
+++ Entity Framework Core/00. ADO.NET/03. Minion Names/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Minion_Names
+{
+    internal class Config
+    {
+        public string path = @"..\..\..\connectionString.txt";
+
+        public string ReturnConnectionString()
+        {
+            string connectionString = null;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                connectionString = reader.ReadToEnd();
+            }
+
+            return connectionString;
+        }
+    }
+}
---
+++ Entity Framework Core/00. ADO.NET/03. Minion Names/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _03._Minion_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = new Config();
+            string connectionString = config.ReturnConnectionString();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                string result = GetVillainsMinionsCount(sqlConnection);
+                Console.WriteLine(result);
+            }
+        }
+        private static string GetVillainsMinionsCount(SqlConnection sqlConnection)
+        {
+            StringBuilder output = new StringBuilder();
+            SqlCommand sqlCommand = new SqlCommand(@"SELECT [v].[Name], COUNT(*) AS [MinionsCount] FROM [Villains] AS [v]
+                                                       JOIN [MinionsVillains] AS [mv] ON [mv].VillainId = [v].Id
+                                                       GROUP BY [v].[Name]
+                                                       HAVING COUNT(*) > 3
+                                                       ORDER BY [MinionsCount]", sqlConnection);
+
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string villainName = (string)reader["Name"];
+                    int minionsUnderControl = (int)reader["MinionsCount"];
+                    output.AppendLine($"{villainName} - {minionsUnderControl}");
+                }
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
