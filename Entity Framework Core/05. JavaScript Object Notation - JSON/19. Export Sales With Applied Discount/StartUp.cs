ea46dd7862002a1a90aff2a63056a4d9eba1518d
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-22 14:31:29 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/19. Export Sales With Applied Discount/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetSalesWithAppliedDiscount(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            var sales = context.Sales
+                .Take(10)
+                .Select(s => new
+                {
+                    car = new
+                    {
+                        Make = s.Car.Make,
+                        Model = s.Car.Model,
+                        TraveledDistance = s.Car.TraveledDistance
+                    },
+                    customerName = s.Customer.Name,
+                    discount = $"{s.Discount:F2}",
+                    price = $"{s.Car.PartsCars.Sum(pc => pc.Part.Price):F2}",
+                    priceWithDiscount = $"{s.Car.PartsCars.Sum(pc => pc.Part.Price) * (1 - (s.Discount / 100)):F2}",
+                })
+                .ToArray();
+
+            string salesToJson = JsonConvert.SerializeObject(sales, Formatting.Indented);
+            return salesToJson;
+        }
+    }
+}
