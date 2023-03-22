9f991c2770ae93d0f72d70c3884a50d33fc6fd16
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-22 14:31:17 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/17. Export Cars With Their List Of Parts/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetCarsWithTheirListOfParts(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context.Cars
+               .Select(c => new
+               {
+                   car = new
+                   {
+                       c.Make,
+                       c.Model,
+                       c.TraveledDistance
+                   },
+                   parts = c.PartsCars.Select(p => new
+                   {
+                       p.Part.Name,
+                       Price = $"{p.Part.Price:F2}"
+                   })
+               });
+
+            string carsToJson = JsonConvert.SerializeObject(cars, Formatting.Indented);
+            return carsToJson;
+        }
+    }
+}
