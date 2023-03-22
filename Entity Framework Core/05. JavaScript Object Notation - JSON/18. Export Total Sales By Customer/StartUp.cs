9b41f1b25534081ec5f241a4f2a467d2e4cc699a
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-22 14:31:22 +0200

    .

---
+++ Entity Framework Core/05. JavaScript Object Notation - JSON/18. Export Total Sales By Customer/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (CarDealerContext dbContext = new CarDealerContext())
+            {
+                string result = GetTotalSalesByCustomer(dbContext);
+                Console.WriteLine(result);
+            }
+        }
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .Where(c => c.Sales.Count() > 0)
+                .Select(c => new
+                {
+                    fullName = c.Name,
+                    boughtCars = c.Sales.Count(),
+                    spentMoney = c.Sales.Sum(s => s.Car.PartsCars.Sum(pc => pc.Part.Price))
+                })
+                .OrderByDescending(c => c.spentMoney)
+                .ThenByDescending(c => c.boughtCars)
+                .ToArray();
+
+            string customersToJson = JsonConvert.SerializeObject(customers, Formatting.Indented);
+            return customersToJson;
+        }
+    }
+}
