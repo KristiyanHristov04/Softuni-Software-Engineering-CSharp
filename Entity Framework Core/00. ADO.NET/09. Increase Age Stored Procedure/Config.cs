d6cae21e99a0c82d748fe984562104c68fa5452f
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-03-22 14:29:29 +0200

    .

---
+++ Entity Framework Core/00. ADO.NET/09. Increase Age Stored Procedure/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._Increase_Age_Stored_Procedure
+{
+    internal class Config
+    {
+        public string path = @"..\..\..\connectionString.txt";
+
+        public string ReturnConnectionString()
+        {
+            string connectionString = null;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                connectionString = reader.ReadToEnd();
+            }
+
+            return connectionString;
+        }
+    }
+}
---
+++ Entity Framework Core/00. ADO.NET/09. Increase Age Stored Procedure/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _09._Increase_Age_Stored_Procedure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = new Config();
+            string connectionString = config.ReturnConnectionString();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                int minionId = Convert.ToInt32(Console.ReadLine());
+                string result = IncreaseAge(sqlConnection, minionId);
+                Console.WriteLine(result);
+            }
+        }
+        private static string IncreaseAge(SqlConnection sqlConnection, int minionId)
+        {
+            StringBuilder output = new StringBuilder();
+            SqlCommand sqlCommand01 = new SqlCommand(@"EXEC [dbo].[usp_GetOlder] @minionId", sqlConnection);
+            sqlCommand01.Parameters.AddWithValue("@minionId", minionId);
+            sqlCommand01.ExecuteNonQuery();
+
+            SqlCommand sqlCommand02 = new SqlCommand(@"SELECT [Name], [Age] FROM [Minions] WHERE [Id] = @Id", sqlConnection);
+            sqlCommand02.Parameters.AddWithValue("@Id", minionId);
+            using (SqlDataReader reader = sqlCommand02.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    output.AppendLine($"{reader["Name"]} – {reader["Age"]} years old");
+                }
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
