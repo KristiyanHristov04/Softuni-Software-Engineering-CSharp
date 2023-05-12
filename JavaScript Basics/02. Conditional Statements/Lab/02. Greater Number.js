bf12ba80fcec148aa121bc96dfbffc9e2e7d3173
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-05-12 13:33:18 +0300

    Task 02

---
+++ JavaScript Basics/02. Conditional Statements/Lab/02. Greater Number.js
@@ -0,0 +1,13 @@
+function greaterNumber(input){
+    let firstNumber = Number(input[0]);
+    let secondNumber = Number(input[1]);
+
+    if (firstNumber > secondNumber) {
+        console.log(firstNumber);
+    }
+    else{
+        console.log(secondNumber);
+    }
+}
+
+// greaterNumber(['10', '10']);
