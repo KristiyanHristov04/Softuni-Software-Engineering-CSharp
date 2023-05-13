083fef4a9aad0686ab4f69cdafcefd5d08fe7eea
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-05-13 12:21:20 +0300

    Task 03

---
+++ JavaScript Basics/02. Conditional Statements/Lab/03. Even or Odd.js
@@ -0,0 +1,10 @@
+function oddOrEven(input){
+    let number = Number(input[0]);
+    if (number % 2 == 0) {
+        console.log('even');
+    } else {
+        console.log('odd');
+    }
+}
+
+// oddOrEven(['6']);
