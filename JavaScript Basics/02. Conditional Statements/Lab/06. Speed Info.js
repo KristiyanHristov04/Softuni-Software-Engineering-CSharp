dc722d8af016645d16c4705d6e0a7bb58a4a8bc4
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-05-15 01:02:48 +0300

    Task 06

---
+++ JavaScript Basics/02. Conditional Statements/Lab/06. Speed Info.js
@@ -0,0 +1,20 @@
+function solve(input){
+    let speed = Number(input[0]);
+    if (speed <= 10) {
+        console.log('slow');
+    }
+    else if(speed > 10 && speed <= 50){
+        console.log('average');
+    }
+    else if(speed > 50 && speed <= 150){
+        console.log('fast');
+    }
+    else if(speed > 150 && speed <= 1000){
+        console.log('ultra fast');
+    }
+    else if(speed > 1000){
+        console.log('extremely fast');
+    }
+}
+
+// solve(['49.5']);
