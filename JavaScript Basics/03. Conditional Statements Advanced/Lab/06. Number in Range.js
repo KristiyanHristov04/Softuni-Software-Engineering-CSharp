f6f0e2ebbc13c9e40599e612384d21f10f374865
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-05-20 11:34:10 +0300

    Task 06

---
+++ JavaScript Basics/03. Conditional Statements Advanced/Lab/06. Number in Range.js
@@ -0,0 +1,11 @@
+function solve(input){
+    let number = Number(input[0]);
+    if(number >= -100 && number <= 100 && number != 0){
+        console.log('Yes');
+    }
+    else{
+        console.log('No');
+    }
+}
+
+// solve(['0']);
