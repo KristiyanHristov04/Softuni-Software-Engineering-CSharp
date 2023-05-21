02b9cf391937938c52cf5f2852ad911b741340b3
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-05-21 12:15:25 +0300

    Task 10

---
+++ JavaScript Basics/03. Conditional Statements Advanced/Lab/10. Invalid Number.js
@@ -0,0 +1,9 @@
+function solve(input){
+    let number = Number(input[0]);
+
+    if(!((number >= 100 && number <= 200) || number == 0)){
+        console.log('invalid');
+    }
+}
+
+solve(["75"]);
