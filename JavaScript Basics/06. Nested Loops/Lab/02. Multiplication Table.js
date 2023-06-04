4be7a1ce29cad10f638d05da5c2f837f454cd2d7
Kristiyan <kristiyan_hristov04@abv.bg>
2023-06-04 13:28:27 +0300

    Task 02

---
+++ JavaScript Basics/06. Nested Loops/Lab/02. Multiplication Table.js
@@ -0,0 +1,10 @@
+function solve(){
+    for(let i = 1; i <= 10; i++){
+        for(let j = 1; j <= 10; j++){
+            let result = i * j;
+            console.log(`${i} * ${j} = ${result}`);
+        }
+    }
+}
+
+// solve();
