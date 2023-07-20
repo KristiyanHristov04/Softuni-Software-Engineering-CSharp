2b6eebb2bda575e486dabed17370eacb21ac9af4
Kristiyan <kristiyan_hristov04@abv.bg>
2023-07-20 11:26:30 +0300

    Task 05

---
+++ JS Front-End/02. Functions and Statements/More Exercises/05. Print DNA.js
@@ -0,0 +1,32 @@
+function solve(number) {
+
+    let str = "ATCGTTAGGG";
+  
+    let counter = 0;
+  
+    for (let i = 0; i < number; i++) {
+  
+      if (i % 4 == 0) {
+  
+        console.log(`**${str[counter % 10]}${str[(counter % 10) + 1]}**`);
+  
+      } else if (i % 4 == 1) {
+  
+        console.log(`*${str[counter % 10]}--${str[(counter % 10) + 1]}*`);
+  
+      } else if (i % 4 == 2) {
+  
+        console.log(`${str[counter % 10]}----${str[(counter % 10) + 1]}`);
+  
+      } else if (i % 4 == 3) {
+  
+        console.log(`*${str[counter % 10]}--${str[(counter % 10) + 1]}*`);
+  
+      }
+  
+      counter += 2;
+  
+    }
+}
+
+solve(5);
