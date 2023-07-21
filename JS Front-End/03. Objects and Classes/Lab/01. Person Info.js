090450efbde30099d1eb1fb8d1b9dcfaef429a1f
Kristiyan <kristiyan_hristov04@abv.bg>
2023-07-21 11:31:15 +0300

    Task 01

---
+++ JS Front-End/03. Objects and Classes/Lab/01. Person Info.js
@@ -0,0 +1,6 @@
+function personInfo(firstName, lastName, age) {
+    let person = { firstName: firstName, lastName: lastName, age: age };
+    return person;
+}
+
+console.log(personInfo('Peter', 'Pan', '20'));
