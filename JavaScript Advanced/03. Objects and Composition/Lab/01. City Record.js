961917d5511a6b1adcc9856d8c92261dc2f72c23
Kristiyan <kristiyan_hristov04@abv.bg>
2023-08-28 14:13:59 +0300

    Task 01

---
+++ JavaScript Advanced/03. Objects and Composition/Lab/01. City Record.js
@@ -0,0 +1,13 @@
+function solve(name, population, treasury) {
+    let object = {
+        name,
+        population,
+        treasury
+    }
+
+    return object;
+}
+
+solve('Tortuga',
+7000,
+15000);
