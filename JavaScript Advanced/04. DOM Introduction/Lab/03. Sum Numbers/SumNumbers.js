2309622de95eccb732b8507df690907c02fa9a6c
Kristiyan <kristiyan_hristov04@abv.bg>
2023-09-02 18:16:43 +0300

    Task 03

---
+++ JavaScript Advanced/04. DOM Introduction/Lab/03. Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    let firstInput = document.getElementById('num1');
+    let secondInput = document.getElementById('num2');
+    let sum = document.getElementById('sum');
+    sum.value = Number(firstInput.value) + Number(secondInput.value);
+}
