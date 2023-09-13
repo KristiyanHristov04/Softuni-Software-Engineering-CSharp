df14034fbc91782037101d58e033f52f8b3abda5
Kristiyan <kristiyan_hristov04@abv.bg>
2023-09-13 11:27:12 +0300

    Task 01

---
+++ JavaScript Advanced/08. Classes and Attributes/Lab/01. Person.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+const person = new Person('Kristiyan', 'Hristov', 19, 'test@abv.bg');
+console.log(person.toString());
