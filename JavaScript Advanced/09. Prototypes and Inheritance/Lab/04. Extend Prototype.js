9f75417cd260f157d83e9f3bc58e1b425a84b23f
Kristiyan <kristiyan_hristov04@abv.bg>
2023-09-18 12:52:11 +0300

    Task 04

---
+++ JavaScript Advanced/09. Prototypes and Inheritance/Lab/04. Extend Prototype.js
@@ -0,0 +1,7 @@
+function extendProrotype(classToExtend) {
+    // TODO:
+    classToExtend.prototype.species = 'Human';
+    classToExtend.prototype.toSpeciesString = function () {
+        return `I am a ${this.species}. ${this.toString()}`;
+    }
+}
