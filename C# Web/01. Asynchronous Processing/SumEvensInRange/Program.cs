c460d28fab62f8f51534ffd7045118561b836cb2
Kristiyan <kristiyan_hristov04@abv.bg>
2023-09-26 15:00:49 +0300

    Sum Even Numbers In Range

---
+++ C# Web/01. Asynchronous Processing/SumEvensInRange/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SumEvensInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+
+            Task task = Task.Run(async () =>
+            {
+                for (long i = 0; i < 1000000000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                    await Task.Delay(1000);
+                }
+            });
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "show")
+                {
+                    Console.WriteLine(sum);
+                } else if (input == "exit")
+                {
+                    return;
+                }
+            }
+        }
+
+    }
+}
