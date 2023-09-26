4ee7915a33e0368e13c80060ce0eb607ffc630f7
Kristiyan <kristiyan_hristov04@abv.bg>
2023-09-26 14:59:14 +0300

    Async Processing Task

---
+++ C# Web/01. Asynchronous Processing/Asynchronous Processing/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Asynchronous_Processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            Thread myThread = new Thread(() =>
+            {
+                PrintEvenNumbers(start, end);
+            });
+
+            myThread.Start();
+            myThread.Join();
+            Console.WriteLine("Thread finished work");
+        }
+
+        static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
