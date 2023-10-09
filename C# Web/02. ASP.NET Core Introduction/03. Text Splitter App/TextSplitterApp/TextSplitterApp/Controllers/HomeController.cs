11b2294be27ada6feca22e718b4b652925dc1920
Kristiyan <kristiyan_hristov04@abv.bg>
2023-10-09 19:49:39 +0300

    Third Task - Text Splitter App

---
+++ C# Web/02. ASP.NET Core Introduction/03. Text Splitter App/TextSplitterApp/TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model)
+            => View(model);
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splitTextArray = model
+                .Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            model.SplitText = string.Join("\r\n", splitTextArray);
+
+            return RedirectToAction("Index", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C# Web/02. ASP.NET Core Introduction/03. Text Splitter App/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models
+{
+    public class TextViewModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string Text { get; set; } = null!;
+
+        public string SplitText { get; set; } = null!;
+    }
+}
