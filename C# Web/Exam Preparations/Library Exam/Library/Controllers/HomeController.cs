5fba81197b5892a2f0777452ed91f4a06e94b6c7
Kristiyan <kristiyan_hristov04@abv.bg>
2023-10-19 13:27:28 +0300

    Moved in new folder

---
+++ C# Web/Exam Preparations/Library Exam/Library/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity!.IsAuthenticated)
+            {
+                return RedirectToAction("All", "Book");
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C# Web/Exam Preparations/Library Exam/Library/Data/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using static Library.Common.Constants.Book;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(AuthorMaxLength)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
