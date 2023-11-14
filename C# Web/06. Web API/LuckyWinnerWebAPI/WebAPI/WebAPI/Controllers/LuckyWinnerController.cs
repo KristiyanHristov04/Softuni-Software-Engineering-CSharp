529ca051161c4db1b2306f0d1b2d40909fbfdf8d
Kristiyan <kristiyan_hristov04@abv.bg>
2023-11-14 20:00:00 +0200

    Lucky Winner Web API CRUD Operations

---
+++ C# Web/06. Web API/LuckyWinnerWebAPI/WebAPI/WebAPI/Controllers/LuckyWinnerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuckyWinnerController : ControllerBase
+    {
+        private static readonly List<Person> People = new List<Person>()
+        {
+            new Person("Kristiyan", "Krasimirov", "Hristov", 19, "Veliko Tarnovo", "Bulgaria", true),
+            new Person("Maria", "Magdalena", "Ivanova", 22, "Melnik", "Bulgaria", false)
+        };
+
+        [HttpGet]
+        public IActionResult GetWinner()
+        {
+            int luckyWinner = Random.Shared.Next(0, People.Count);
+            return Ok(People[luckyWinner]);
+        }
+
+        [HttpPost]
+        public IActionResult PostPerson(string firstName, string middleName, string lastName, int age, string town, string country, bool isMale)
+        {
+            People.Add(new Person(firstName, middleName, lastName, age, town, country, isMale));
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult PutPerson(int id, string firstName, string middleName, string lastName, int age, string town, string country, bool isMale)
+        {
+            Person? personToEdit = People.FirstOrDefault(p => p.PersonId == id);
+            if (personToEdit == null)
+            {
+                return NotFound();
+            }
+
+            personToEdit.FirstName = firstName;
+            personToEdit.MiddleName = middleName;
+            personToEdit.LastName = lastName;
+            personToEdit.Age = age;
+            personToEdit.Town = town;
+            personToEdit.Country = country;
+            personToEdit.IsMale = isMale;
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeletePerson(int id)
+        {
+            Person? personToDelete = People.FirstOrDefault(p => p.PersonId == id);
+            if (personToDelete == null)
+            {
+                return NotFound();
+            }
+
+            People.Remove(personToDelete);
+
+            return Ok();
+        }
+    }
+}
---
+++ C# Web/06. Web API/LuckyWinnerWebAPI/WebAPI/WebAPI/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace WebAPI
+{
+    public class Person
+    {
+        private static int personId = 1;
+        private int currentPersonId;
+        public Person(string firstName, string middleName, string lastName,
+            int age, string town, string country, bool isMale)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Town = town;
+            this.Country = country;
+            IsMale = isMale;
+            currentPersonId = personId;
+            personId++;
+        }
+
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int Age { get; set; }
+        public string Town { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public bool IsMale { get; set; }
+        public int PersonId
+        {
+            get { return this.currentPersonId; }
+        }
+    }
+}
