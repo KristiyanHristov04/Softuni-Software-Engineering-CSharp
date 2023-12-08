9c01552cc4ef4893c83b9607eeb508cd9d8c76d9
Kristiyan <kristiyan_hristov04@abv.bg>
2023-12-08 20:01:28 +0200

    Update Version Of Task Locked Profile

--- JS Front-End/05. HTTP and REST/More Exercises/01. Locked Profile/app.js
+++ JS Front-End/05. HTTP and REST/More Exercises/01. Locked Profile/app.js
@@ -1,56 +1,65 @@
 function lockedProfile() {
+    const URL = 'http://localhost:3030/jsonstore/advanced/profiles';
     const main = document.getElementById('main');
-    const profile = document.getElementsByClassName('profile')[0];
-    main.textContent = '';
-    const URL = `http://localhost:3030/jsonstore/advanced/profiles`;
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            let peopleData = Object.values(data);
-            for (let index = 0; index < peopleData.length; index++) {
-                let username = peopleData[index].username;
-                let email = peopleData[index].email;
-                let age = peopleData[index].age;
-                let newProfile = profile.cloneNode(true);
-                let profileInfo = Array.from(newProfile.children);
-                let radioInput01 = profileInfo[2];
-                let radioInput02 = profileInfo[4];
-                radioInput01.name = `user${index + 1}Locked`;
-                radioInput02.name = `user${index + 1}Locked`;
-                let usernameInput = profileInfo[8];
-                usernameInput.name = `user${index + 1}Username`;
-                usernameInput.value = username;
-                let divHiddenInfo = profileInfo[9];
-                divHiddenInfo.classList.remove('user1Username');
-                divHiddenInfo.classList.add(`user${index + 1}Username`);
-                let hiddenDivChildren = divHiddenInfo.children;
-                let emailInput = hiddenDivChildren[2];
-                emailInput.name = `user${index + 1}Email`;
-                emailInput.value = email;
-                let ageInput = hiddenDivChildren[4];
-                ageInput.name = `user${index + 1}Age`;
-                ageInput.value = age;
-                divHiddenInfo.style.display = 'none';
-                let showMoreButton = profileInfo[10];
-                showMoreButton.addEventListener('click', showMoreHideButtonHandler);
-                main.appendChild(newProfile);
-            }
-        })
-        .catch(error => console.error(error));
+    
+    loadUsers();
 
-        function showMoreHideButtonHandler(event) {
-            let parent = event.currentTarget.parentElement;
-            let isProfileLocked = parent.children[2].checked;
-            let buttonText = event.currentTarget.textContent;
-            if (!isProfileLocked && buttonText === 'Show more') {
-                parent.children[9].style.display = 'block';
-                event.currentTarget.textContent = 'Hide it';
-                console.log(parent.children[8].value);
-                console.log(parent.children[9].children[2].value);
-                console.log(parent.children[9].children[4].value);
-            } else if (!isProfileLocked && buttonText === 'Hide it') {
-                parent.children[9].style.display = 'none';
-                event.currentTarget.textContent = 'Show more';
+    function loadUsers() {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                let users = Object.values(data);
+                let currentUser = 1;
+                for (const user of users) {
+                    let username = user.username;
+                    let email = user.email;
+                    let age = user.age;
+                    let id = user._id;
+                    loadProfile(username, email, age, id, currentUser);
+                    currentUser++;
+                }
+                main.children[0].remove();
+            })
+            .catch(error => console.log(error));
+    }
+
+    function loadProfile(username, email, age, id, currentUser) {
+        let profile = document.getElementsByClassName('profile')[0].cloneNode(true);
+
+        let profileChildren = profile.children;
+
+        profileChildren[2].name = `user${currentUser}Locked`;
+        profileChildren[4].name = `user${currentUser}Locked`;
+        profileChildren[8].name = `user${currentUser}Username`;
+        profileChildren[8].value = username;
+        profileChildren[9].setAttribute('class', `user${currentUser}Username`);
+        let button = profileChildren[10];
+
+        button.addEventListener('click', (e) => {
+            let parentElement = e.currentTarget.parentElement;
+            let radioButton = parentElement.children[2];
+
+            if (!radioButton.checked && e.currentTarget.textContent === 'Show more') {
+                parentElement.children[9].style.display = 'block';
+                e.currentTarget.textContent = 'Hide it';
+            } else if (!radioButton.checked && e.currentTarget.textContent === 'Hide it') {
+                parentElement.children[9].style.display = 'none';
+                e.currentTarget.textContent = 'Show more';
             }
-        }
+        });
+        
+        let divChildren = profileChildren[9].children;
+
+        divChildren[2].name = `user${currentUser}Email`;
+        divChildren[2].value = email;
+
+        divChildren[4].type = 'email';
+
+        divChildren[4].value = age;
+
+        profileChildren[9].style.display = 'none';
+
+        profile.id = id;
+        main.appendChild(profile);
+    }
 }
