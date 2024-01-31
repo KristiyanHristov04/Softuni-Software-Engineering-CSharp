dd6628382f2656e30c7a627ee31284ecc8899b0c
Kristiyan <kristiyan_hristov04@abv.bg>
2024-01-31 11:29:13 +0200

    Basic SignalR ChatApp

---
+++ C# Web/07. SignalR/ChatApp/WebApplication1/WebApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
